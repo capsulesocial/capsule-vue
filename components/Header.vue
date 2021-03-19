b2e2e6b3cca38bb266df92212f32be779b55c6b6
jackdishman <jdish123@gmail.com>
2021-03-19 15:41:05 -0700

    Sidenav tabs now change colors based on current state. Added Search Bar.

--- components/Header.vue
+++ components/Header.vue
@@ -1,9 +1,18 @@
 <template>
   <section class="flex justify-between p-4">
     <CapsuleIcon class="py-4" />
-    <button>
-      Search
-    </button>
+
+    <article class="rounded-full bg-gray-100 w-3/5 m-4 flex flex-start">
+      <button type="submit" class="px-4">
+        <SearchIcon />
+      </button>
+      <input
+        type="search"
+        placeholder="Type your keywords..."
+        class="bg-gray-100 text-black focus:outline-none flex-grow mr-10"
+      />
+    </article>
+
     <nuxt-link to="/profile">
       Profile
     </nuxt-link>
@@ -11,11 +20,13 @@
 </template>
 
 <script>
-import CapsuleIcon from "@/components/CapsuleIcon";
+import CapsuleIcon from "@/components/icons/Capsule";
+import SearchIcon from "@/components/icons/Search";
 export default {
   setup() {},
   components: {
-    CapsuleIcon
+    CapsuleIcon,
+    SearchIcon
   }
 };
 </script>
--- components/Navigation.vue
+++ components/Navigation.vue
@@ -2,38 +2,70 @@
   <nav class="px-2 flex flex-col">
     <nuxt-link
       to="/home"
-      class="group flex items-center px-2 py-2 text-base leading-6 font-bold rounded-full text-primary"
-      >Home</nuxt-link
+      class="group flex items-center px-2 py-2 text-base leading-6"
     >
+      <span v-if="this.$route.path === '/home'" class="font-bold text-primary"
+        >Home</span
+      >
+      <span v-else class="font-semibold">Home</span>
+    </nuxt-link>
     <nuxt-link
       to="/discover"
       class="mt-1 group flex items-center px-2 py-2 text-base leading-6 font-semibold"
-      >Discover</nuxt-link
     >
+      <span
+        v-if="this.$route.path === '/discover'"
+        class="font-bold text-primary"
+        >Discover</span
+      >
+      <span v-else class="font-semibold">Discover</span>
+    </nuxt-link>
     <nuxt-link
       to="/messages"
       class="mt-1 group flex items-center px-2 py-2 text-base leading-6 font-semibold"
-      >Messages</nuxt-link
     >
+      <span
+        v-if="this.$route.path === '/messages'"
+        class="font-bold text-primary"
+        >Messages</span
+      >
+      <span v-else class="font-semibold">Messages</span>
+    </nuxt-link>
     <nuxt-link
       to="/profile"
       class="mt-1 group flex items-center px-2 py-2 text-base leading-6 font-semibold"
-      >Profile</nuxt-link
     >
+      <span
+        v-if="this.$route.path === '/profile'"
+        class="font-bold text-primary"
+        >Profile</span
+      >
+      <span v-else class="font-semibold">Profile</span>
+    </nuxt-link>
     <nuxt-link
       to="/settings"
       class="mt-1 group flex items-center px-2 py-2 text-base leading-6 font-semibold"
-      >Settings</nuxt-link
     >
+      <span
+        v-if="this.$route.path === '/settings'"
+        class="font-bold text-primary"
+        >Settings</span
+      >
+      <span v-else class="font-semibold">Settings</span>
+    </nuxt-link>
     <nuxt-link
       to="/help"
       class="mt-1 group flex items-center px-2 py-2 text-base leading-6 font-semibold"
-      >Help</nuxt-link
     >
+      <span v-if="this.$route.path === '/help'" class="font-bold text-primary"
+        >Help</span
+      >
+      <span v-else class="font-semibold">Help</span>
+    </nuxt-link>
     <button
       class="bg-primary w-48 mt-5 hover:bg-green-600 text-white font-bold py-2 px-4 rounded-lg shadow-lg"
     >
-      Post
+      Write Post
     </button>
     <div
       class="mt-1 group flex items-center px-2 py-2 text-base leading-6 font-semibold"
@@ -44,3 +76,7 @@
     </div>
   </nav>
 </template>
+
+<script>
+export default {};
+</script>
