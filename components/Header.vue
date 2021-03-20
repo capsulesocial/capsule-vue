175f1f3f2e7e8d5a8df89fe81ab2dbf7dfa29cd0
jackdishman <jdish123@gmail.com>
2021-03-19 18:10:41 -0700

    Converted the Search Bar into a component

--- components/Header.vue
+++ components/Header.vue
@@ -1,32 +1,19 @@
 <template>
   <section class="flex justify-between p-4">
     <CapsuleIcon class="py-4" />
-
-    <article class="rounded-full bg-gray-100 w-3/5 m-4 flex flex-start">
-      <button type="submit" class="px-4">
-        <SearchIcon />
-      </button>
-      <input
-        type="search"
-        placeholder="Type your keywords..."
-        class="bg-gray-100 text-black focus:outline-none flex-grow mr-10"
-      />
-    </article>
-
-    <nuxt-link to="/profile">
-      Profile
-    </nuxt-link>
+    <SearchBar />
+    <nuxt-link to="/profile"> Profile </nuxt-link>
   </section>
 </template>
 
 <script>
 import CapsuleIcon from "@/components/icons/Capsule";
-import SearchIcon from "@/components/icons/Search";
+import SearchBar from "@/components/Search";
 export default {
   setup() {},
   components: {
     CapsuleIcon,
-    SearchIcon
+    SearchBar
   }
 };
 </script>
---
+++ components/Search.vue
@@ -0,0 +1,28 @@
+<template>
+  <article class="rounded-full bg-gray-100 w-3/5 m-4 flex flex-start">
+    <button type="submit" class="px-4" @click="submitSearch()">
+      <SearchIcon />
+    </button>
+    <input
+      v-model="keyword"
+      type="search"
+      placeholder="Type your keywords..."
+      class="bg-gray-100 text-black focus:outline-none flex-grow mr-10"
+    />
+  </article>
+</template>
+
+<script>
+import SearchIcon from "@/components/icons/Search";
+
+export default {
+  data() {
+    return {
+      keyword: ""
+    };
+  },
+  components: {
+    SearchIcon
+  }
+};
+</script>
