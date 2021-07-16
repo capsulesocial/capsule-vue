e6783028ed351f8c60da9723a48492e9ed770df7
jack dishman <me@jackdishman.com>
2021-07-16 13:04:52 -0700

    decreased size of header dropdown icons

--- src/components/icons/Help.vue
+++ src/components/icons/Help.vue
@@ -1,8 +1,8 @@
 <template>
   <svg
     xmlns="http://www.w3.org/2000/svg"
-    width="24"
-    height="24"
+    width="20"
+    height="20"
     viewBox="0 0 24 24"
     fill="none"
     stroke="currentColor"
@@ -17,6 +17,5 @@
     <line x1="14.83" y1="14.83" x2="19.07" y2="19.07"></line>
     <line x1="14.83" y1="9.17" x2="19.07" y2="4.93"></line>
     <line x1="14.83" y1="9.17" x2="18.36" y2="5.64"></line>
-    <line x1="4.93" y1="19.07" x2="9.17" y2="14.83"></line>
-  </svg>
+    <line x1="4.93" y1="19.07" x2="9.17" y2="14.83"></line></svg>
 </template>
--- src/components/icons/Search.vue
+++ src/components/icons/Search.vue
@@ -12,5 +12,5 @@
     class="feather feather-search"
   >
     <circle cx="11" cy="11" r="8"></circle>
-    <line x1="21" y1="21" x2="16.65" y2="16.65"></line></svg
-></template>
+    <line x1="21" y1="21" x2="16.65" y2="16.65"></line></svg>
+</template>
