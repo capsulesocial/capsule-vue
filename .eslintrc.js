9288c4a1d86b8733f9af3c1d254af9c630950f06
Christos Panagiotakopoulos <chrispanag@gmail.com>
2021-07-26 12:27:50 +0300

    [style] Add prettier to eslint for enforcing an opinionated code style
    * Added prettier, eslint-config-prettier, eslint-plugin-prettier
      packages
    * Added prettier and plugin:prettier/recommended to the eslint pipeline
    * Created .prettierrc with the following settings: use tabs instead of
      spaces, no semicolons, singleQuote instead of double, line length = 120
      characters

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,7 +6,9 @@ module.exports = {
 		"es2021": true
 	},
 	"extends": [
-		"@nuxtjs/eslint-config-typescript"
+		"@nuxtjs/eslint-config-typescript",
+		'prettier', // Uses eslint-config-prettier to disable ESLint rules from @typescript-eslint/eslint-plugin that would conflict with prettier
+        'plugin:prettier/recommended' // Enables eslint-plugin-prettier and eslint-config-prettier. This will display prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
 	],
 	"rules": {
 		"vue/attribute-hyphenation": 0,
