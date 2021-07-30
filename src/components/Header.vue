4f366a2670715509264f3bfa05d3609220d3aad8
ashutosh1206 <ashutosh.ahelleya@gmail.com>
2021-07-30 21:22:34 +0530

    Remove a user's key from keychain when they logout

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -133,7 +133,10 @@ export default Vue.extend({
 		...mapMutations(sessionStoreNamespace, {
 			endSession: MutationType.LOGOUT,
 		}),
-		logout() {
+		async logout() {
+			const node = this.$getNode()
+			// Remove a user's key from keychain when they logout
+			await node.key.rm(`blogchain-auth-${this.$store.state.session.id}`)
 			this.endSession()
 			this.$router.push(`/`)
 		},
