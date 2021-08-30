fa174ecbfc3f1f2eb308ac34e181f17d592a2398
Christos Panagiotakopoulos <chrispanag@gmail.com>
2021-08-30 18:39:32 +0300

    [Refactor] Some more refactorings done together with Jack (#47)
    * [Refactor] Refactor helpers for dates and solve bug
    
    * [Refactor] Refactor filter comments

--- src/plugins/helpers.ts
+++ src/plugins/helpers.ts
@@ -31,47 +31,56 @@ const getFormat: dateString = (date, preformattedDate = false, hideYear = false)
 	const month = MONTH_NAMES[date.getMonth()]
 	const year = date.getFullYear()
 	const hours = date.getHours()
-	let minutes: any = date.getMinutes()
+	const minutes = date.getMinutes()
 
+	let minutesString = `${minutes}`
 	if (minutes < 10) {
 		// Adding leading zero to minutes
-		minutes = `0${minutes}`
+		minutesString = `0${minutes}`
 	}
+
 	if (preformattedDate) {
 		// Today at 4:20
-		return `${preformattedDate} at ${hours}:${minutes}`
+		return `${preformattedDate} at ${hours}:${minutesString}`
 	}
+
 	if (hideYear) {
-		return `${day}. ${month} at ${hours}:${minutes}`
+		return `${day}. ${month} at ${hours}:${minutesString}`
 	}
-	return `${day}. ${month} ${year}. at ${hours}:${minutes}`
+
+	return `${day}. ${month} ${year}. at ${hours}:${minutesString}`
 }
 
-const formatDate = (input) => {
-	if (!input) {
-		return null
-	}
-	const date = typeof input === `object` ? input : new Date(input)
+const formatDate = (input: string | Date | number) => {
+	const date = input instanceof Date ? input : new Date(input)
 	const DAY_IN_MS = 86400000 // 24 * 60 * 60 * 1000
-	const today: any = new Date()
-	const yesterday = new Date(today - DAY_IN_MS)
-	const seconds = Math.round((today - date) / 1000)
-	const minutes = Math.round(seconds / 60)
-	const isToday = today.toDateString() === date
-	const isYesterday = yesterday.toDateString() === date.toDateString()
-	const isThisYear = today.getFullYear() === date.getFullYear()
+	const today = new Date()
+	const seconds = Math.round((today.getTime() - date.getTime()) / 1000)
 
 	if (seconds < 5) {
 		return `now`
-	} else if (seconds < 60) {
+	}
+	if (seconds < 60) {
 		return `${seconds} seconds ago`
-	} else if (minutes < 60) {
+	}
+
+	const minutes = Math.round(seconds / 60)
+	if (minutes < 60) {
 		return `${minutes} minutes ago`
-	} else if (isToday) {
+	}
+
+	const yesterday = new Date(today.getTime() - DAY_IN_MS)
+	const isToday = today.toDateString() === date.toDateString()
+	const isYesterday = yesterday.toDateString() === date.toDateString()
+	if (isToday) {
 		return getFormat(date, `Today`, false)
-	} else if (isYesterday) {
+	}
+	if (isYesterday) {
 		return getFormat(date, `Yesterday`, false)
-	} else if (isThisYear) {
+	}
+
+	const isThisYear = today.getFullYear() === date.getFullYear()
+	if (isThisYear) {
 		return getFormat(date, false, true)
 	}
 	return getFormat(date, false, false)
