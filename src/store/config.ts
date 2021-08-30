0cc9ad7207d344995b70eda0be3cbb31ab7a1510
jack dishman <me@jackdishman.com>
2021-08-30 08:02:46 -0700

    Uploading emotion images and added to config

--- src/store/config.ts
+++ src/store/config.ts
@@ -1,8 +1,57 @@
 export const state = () => ({
 	feelings: {
-		positive: [`awe`, `excited`, `happy`, `lol`, `proud`, `touched`],
-		negative: [`sad`, `sick`, `terrified`, `skeptical`, `drama`],
-		neutral: [`goofy`],
+		positive: [
+			`awe`,
+			`excited`,
+			`happy`,
+			`lol`,
+			`proud`,
+			`touched`,
+			`calm`,
+			`determined`,
+			`flirtatious`,
+			`fearless`,
+			`histerical`,
+			`interested`,
+			`intrigued`,
+			`onIt`,
+			`proudMilitant`,
+		],
+		negative: [
+			`sad`,
+			`sick`,
+			`terrified`,
+			`drama`,
+			`aloof`,
+			`barelyInterested`,
+			`fearful`,
+			`hate`,
+			`heartbroken`,
+			`hostile`,
+			`intrepid`,
+			`mad`,
+			`notBuyingIt`,
+			`offended`,
+			`pompous`,
+			`regret`,
+			`slap`,
+		],
+		neutral: [
+			`goofy`,
+			`cringe`,
+			`distant`,
+			`dubious`,
+			`skeptical`,
+			`hesitant`,
+			`incognito`,
+			`seduced`,
+			`serious`,
+			`shocked`,
+			`tripping`,
+			`whine`,
+			`yazz`,
+			`zzz`,
+		],
 	},
 	reactions: {
 		awe: {
@@ -10,19 +59,74 @@ export const state = () => ({
 			image: require(`@/assets/images/reactions/awe.png`),
 			background: require(`@/assets/images/backgrounds/agree.png`),
 		},
+		aloof: {
+			label: `Aloof`,
+			image: require(`@/assets/images/reactions/aloof.png`),
+			background: require(`@/assets/images/backgrounds/disagree.png`),
+		},
 		bored: {
 			label: `Bored`,
 			image: require(`@/assets/images/reactions/bored.png`),
 			background: require(`@/assets/images/backgrounds/disagree.png`),
+		},
+		barelyInterested: {
+			label: `Barely Interested`,
+			image: require(`@/assets/images/reactions/barely-interested.png`),
+			background: require(`@/assets/images/backgrounds/disagree.png`),
+		},
+		calm: {
+			label: `Calm`,
+			image: require(`@/assets/images/reactions/calm.png`),
+			background: require(`@/assets/images/backgrounds/agree.png`),
+		},
+		cringe: {
+			label: `Cringe`,
+			image: require(`@/assets/images/reactions/cringed.png`),
+			background: require(`@/assets/images/backgrounds/disagree.png`),
+		},
+		determined: {
+			label: `Determined`,
+			image: require(`@/assets/images/reactions/determined.png`),
+			background: require(`@/assets/images/backgrounds/agree.png`),
+		},
+		distant: {
+			label: `Distant`,
+			image: require(`@/assets/images/reactions/distant.png`),
+			background: require(`@/assets/images/backgrounds/neutral.png`),
+		},
+		distracted: {
+			label: `Distracted`,
+			image: require(`@/assets/images/reactions/distracted.png`),
+			background: require(`@/assets/images/backgrounds/neutral.png`),
 		},
 		drama: {
 			label: `Dramatic`,
 			image: require(`@/assets/images/reactions/drama.png`),
 			background: require(`@/assets/images/backgrounds/neutral.png`),
 		},
+		dubious: {
+			label: `Dubious`,
+			image: require(`@/assets/images/reactions/dubious.png`),
+			background: require(`@/assets/images/backgrounds/neutral.png`),
+		},
 		excited: {
 			label: `Excited`,
 			image: require(`@/assets/images/reactions/excited.png`),
+			background: require(`@/assets/images/backgrounds/agree.png`),
+		},
+		fearful: {
+			label: `Fearful`,
+			image: require(`@/assets/images/reactions/fearful.png`),
+			background: require(`@/assets/images/backgrounds/disagree.png`),
+		},
+		fearless: {
+			label: `Fearless`,
+			image: require(`@/assets/images/reactions/fearless.png`),
+			background: require(`@/assets/images/backgrounds/agree.png`),
+		},
+		flirtatious: {
+			label: `Flirtatious`,
+			image: require(`@/assets/images/reactions/flirtatious.png`),
 			background: require(`@/assets/images/backgrounds/agree.png`),
 		},
 		goofy: {
@@ -35,20 +139,100 @@ export const state = () => ({
 			image: require(`@/assets/images/reactions/happy.png`),
 			background: require(`@/assets/images/backgrounds/agree.png`),
 		},
+		hate: {
+			label: `Hate`,
+			image: require(`@/assets/images/reactions/hate.png`),
+			background: require(`@/assets/images/backgrounds/disagree.png`),
+		},
+		heartbroken: {
+			label: `Heartbroken`,
+			image: require(`@/assets/images/reactions/heartbroken.png`),
+			background: require(`@/assets/images/backgrounds/disagree.png`),
+		},
+		hesitant: {
+			label: `Hesitant`,
+			image: require(`@/assets/images/reactions/hesitant.png`),
+			background: require(`@/assets/images/backgrounds/neutral.png`),
+		},
+		histerical: {
+			label: `Histerical`,
+			image: require(`@/assets/images/reactions/histerical.png`),
+			background: require(`@/assets/images/backgrounds/agree.png`),
+		},
+		hostile: {
+			label: `Hostile`,
+			image: require(`@/assets/images/reactions/hostile.png`),
+			background: require(`@/assets/images/backgrounds/disagree.png`),
+		},
+		incognito: {
+			label: `Incognito`,
+			image: require(`@/assets/images/reactions/incognito.png`),
+			background: require(`@/assets/images/backgrounds/neutral.png`),
+		},
+		interested: {
+			label: `Interested`,
+			image: require(`@/assets/images/reactions/interested.png`),
+			background: require(`@/assets/images/backgrounds/agree.png`),
+		},
+		intrepid: {
+			label: `Intrepid`,
+			image: require(`@/assets/images/reactions/intrepid.png`),
+			background: require(`@/assets/images/backgrounds/disagree.png`),
+		},
+		intrigued: {
+			label: `Intrigued`,
+			image: require(`@/assets/images/reactions/intrigued.png`),
+			background: require(`@/assets/images/backgrounds/agree.png`),
+		},
 		lol: {
 			label: `Lol`,
 			image: require(`@/assets/images/reactions/lol.png`),
 			background: require(`@/assets/images/backgrounds/agree.png`),
 		},
-		skeptical: {
-			label: `Skeptical`,
+		mad: {
+			label: `Mad`,
+			image: require(`@/assets/images/reactions/mad.png`),
+			background: require(`@/assets/images/backgrounds/disagree.png`),
+		},
+		notBuyingIt: {
+			label: `Not Buying It`,
 			image: require(`@/assets/images/reactions/not-buyin-it.png`),
+			background: require(`@/assets/images/backgrounds/disagree.png`),
+		},
+		offended: {
+			label: `Offended`,
+			image: require(`@/assets/images/reactions/offended.png`),
+			background: require(`@/assets/images/backgrounds/disagree.png`),
+		},
+		onIt: {
+			label: `On it`,
+			image: require(`@/assets/images/reactions/onIt.png`),
+			background: require(`@/assets/images/backgrounds/agree.png`),
+		},
+		pompous: {
+			label: `Pompous`,
+			image: require(`@/assets/images/reactions/pompous.png`),
 			background: require(`@/assets/images/backgrounds/disagree.png`),
 		},
 		proud: {
 			label: `Proud`,
 			image: require(`@/assets/images/reactions/proud.png`),
 			background: require(`@/assets/images/backgrounds/agree.png`),
+		},
+		proudMilitant: {
+			label: `Proud Militant`,
+			image: require(`@/assets/images/reactions/proud-militant.png`),
+			background: require(`@/assets/images/backgrounds/agree.png`),
+		},
+		regret: {
+			label: `Regret`,
+			image: require(`@/assets/images/reactions/regret.png`),
+			background: require(`@/assets/images/backgrounds/disagree.png`),
+		},
+		repugnance: {
+			label: `Repugnance`,
+			image: require(`@/assets/images/reactions/repugnance.png`),
+			background: require(`@/assets/images/backgrounds/disagree.png`),
 		},
 		sad: {
 			label: `Sad`,
@@ -60,9 +244,34 @@ export const state = () => ({
 			image: require(`@/assets/images/reactions/satisfied.png`),
 			background: require(`@/assets/images/backgrounds/agree.png`),
 		},
+		seduced: {
+			label: `Seduced`,
+			image: require(`@/assets/images/reactions/seduced.png`),
+			background: require(`@/assets/images/backgrounds/neutral.png`),
+		},
+		serious: {
+			label: `Serious`,
+			image: require(`@/assets/images/reactions/serious.png`),
+			background: require(`@/assets/images/backgrounds/neutral.png`),
+		},
+		shocked: {
+			label: `Shocked`,
+			image: require(`@/assets/images/reactions/shocked.png`),
+			background: require(`@/assets/images/backgrounds/neutral.png`),
+		},
 		sick: {
 			label: `Sick`,
 			image: require(`@/assets/images/reactions/sick.png`),
+			background: require(`@/assets/images/backgrounds/disagree.png`),
+		},
+		skeptical: {
+			label: `Skeptical`,
+			image: require(`@/assets/images/reactions/skeptical.png`),
+			background: require(`@/assets/images/backgrounds/neutral.png`),
+		},
+		slap: {
+			label: `Slap`,
+			image: require(`@/assets/images/reactions/slap.png`),
 			background: require(`@/assets/images/backgrounds/disagree.png`),
 		},
 		terrified: {
@@ -74,6 +283,26 @@ export const state = () => ({
 			label: `Touched`,
 			image: require(`@/assets/images/reactions/touched.png`),
 			background: require(`@/assets/images/backgrounds/agree.png`),
+		},
+		tripping: {
+			label: `Tripping`,
+			image: require(`@/assets/images/reactions/tripping.png`),
+			background: require(`@/assets/images/backgrounds/neutral.png`),
+		},
+		whine: {
+			label: `Whine`,
+			image: require(`@/assets/images/reactions/whine.png`),
+			background: require(`@/assets/images/backgrounds/neutral.png`),
+		},
+		yazz: {
+			label: `Yazz`,
+			image: require(`@/assets/images/reactions/yazz.png`),
+			background: require(`@/assets/images/backgrounds/agree.png`),
+		},
+		zzz: {
+			label: `Zzz`,
+			image: require(`@/assets/images/reactions/zzz.png`),
+			background: require(`@/assets/images/backgrounds/neutral.png`),
 		},
 	},
 	backgrounds: {
