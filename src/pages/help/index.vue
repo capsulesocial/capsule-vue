88c6d9893e9bbc6ceb8829d2139e288fe23e3041
jack dishman <me@jackdishman.com>
2021-09-15 08:05:58 -0700

    help page

--- src/pages/help/index.vue
+++ src/pages/help/index.vue
@@ -1,3 +1,20 @@
 <template>
-	<main class="w-full">Help</main>
+	<main class="border-l border-r" style="width: 750px; height: calc(100vh - 95px)">
+		<section>
+			<img src="~/assets/images/brand/help-page.png" alt="" class="w-full" />
+		</section>
+		<section>
+			<!-- Page title -->
+			<article class="px-5">
+				<h2 class="text-3xl font-sans font-semibold">Need Some Help?</h2>
+			</article>
+			<article class="bg-lightPrimary bg-opacity-25 mt-5 p-10">
+				<h6 class="text-lg text-lightPrimary pb-5">Using Capsule Social</h6>
+				<h6 class="text-lg text-lightPrimary pb-5">Manage My Account</h6>
+				<h6 class="text-lg text-lightPrimary pb-5">Fix a problem</h6>
+				<h6 class="text-lg text-lightPrimary pb-5">Policies and Reporting</h6>
+				<h6 class="text-lg text-lightPrimary">Chat with Support</h6>
+			</article>
+		</section>
+	</main>
 </template>
