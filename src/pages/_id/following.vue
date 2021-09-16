74677812c118831afc1b2babdc1332bbd2698fa9
jack dishman <me@jackdishman.com>
2021-09-15 22:08:03 -0700

    Fetching & raw data posted on following and followers id pages

--- src/pages/_id/following.vue
+++ src/pages/_id/following.vue
@@ -1,22 +1,49 @@
 <template>
 	<section v-if="profile !== null" class="px-4">
 		<article>
-			<div v-for="i in profile.following" :key="i">
-				<nuxt-link class="text-primary underline" :to="'/' + i">
-					{{ i }}
+			<div v-for="p in profiles" :key="p.id">
+				<nuxt-link class="text-primary underline" :to="'/' + p.id">
+					{{ p }}
 				</nuxt-link>
 			</div>
 		</article>
 	</section>
 </template>
 
-<script>
-export default {
+<script lang="ts">
+import Vue from 'vue'
+import { getFollowersAndFollowing } from '@/backend/following'
+import { getProfile } from '@/backend/profile'
+interface IData {
+	isLoading: boolean
+	profiles: any
+}
+
+export default Vue.extend({
 	props: {
 		profile: {
 			type: Object,
 			default: null,
 		},
 	},
-}
+	data(): IData {
+		return {
+			isLoading: true,
+			profiles: [],
+		}
+	},
+	async created() {
+		const res = await getFollowersAndFollowing(this.$route.params.id)
+		const following = res.following
+		following.forEach(this.getFollowing)
+	},
+	methods: {
+		async getFollowing(p: string) {
+			const profile = await getProfile(p)
+			if (profile) {
+				this.profiles.push(profile)
+			}
+		},
+	},
+})
 </script>
