59b96b1fb3be7f02aeaf4c6cc5d0794f64b96768
jack dishman <me@jackdishman.com>
2021-09-28 18:22:55 -0700

    Infinite scroll has duplicates

--- src/pages/_id/comments.vue
+++ src/pages/_id/comments.vue
@@ -40,14 +40,14 @@ export default Vue.extend({
 		}
 	},
 	async created() {
-		// window.addEventListener(`scroll`, this.handleScroll)
+		window.addEventListener(`scroll`, this.handleScroll)
 		// Fetch comments from Orbit DB by ID
 		this.comments = await getCommentsOfUser(this.$props.profile.id)
 		this.currentOffset += this.limit
 		this.isLoading = false
 	},
 	destroyed() {
-		// window.removeEventListener(`scroll`, this.handleScroll)
+		window.removeEventListener(`scroll`, this.handleScroll)
 	},
 	methods: {
 		async loadComments() {
