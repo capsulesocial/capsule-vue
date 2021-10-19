8247442d59b16b15d94380513eb93daf3afc770d
jack dishman <me@jackdishman.com>
2021-10-19 11:20:34 -0700

    Display bookmarks on profile page

--- src/pages/_id/bookmarks.vue
+++ src/pages/_id/bookmarks.vue
@@ -1,6 +1,6 @@
 <template>
 	<section class="px-4">
-		<article v-for="post in posts" :key="post">
+		<article v-for="post in posts" :key="post._id">
 			<PostCard :post="post" />
 		</article>
 	</section>
@@ -11,10 +11,17 @@ import Vue from 'vue'
 import type { PropType } from 'vue'
 import PostCard from '@/components/post/Card.vue'
 import { Profile } from '@/backend/profile'
-import { getBookmarks } from '@/backend/bookmarks'
+import { getPosts } from '@/backend/post'
 
 interface IData {
-	posts: []
+	posts: {
+		authorID: string
+		category: string
+		excerpt: string
+		tags: any
+		timestamp: number
+		_id: string
+	}[]
 }
 
 export default Vue.extend({
@@ -33,15 +40,13 @@ export default Vue.extend({
 		}
 	},
 	async created() {
-		const bookmarkList: { authorID: string; postCID: string; timestamp: Date }[] = await getBookmarks({
-			authorID: this.$route.params.id,
-		})
+		const bookmarkList = await getPosts({ bookmarkedBy: this.$route.params.id }, this.$store.state.session.id)
 		bookmarkList.forEach((p) => {
-			console.log(p)
+			this.posts.push(p.post)
 		})
 	},
 	methods: {
-		getBookmarks,
+		getPosts,
 	},
 })
 </script>
