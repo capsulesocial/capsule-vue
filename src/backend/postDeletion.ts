2b58fde2ba7fb68c11a5745a512129b170aeb82b
Rahul Trivedi <32861078+rahultrivedi180@users.noreply.github.com>
2021-10-21 15:56:44 +0200

    Add backend post deletion API call (#181)
    * Add backend post deletion API call
    
    * Modified post deletion request body

---
+++ src/backend/postDeletion.ts
@@ -0,0 +1,24 @@
+import axios from 'axios'
+import { signContent } from './keys'
+import { capsuleOrbit } from './utilities/config'
+import { uint8ArrayToHexString } from './utilities/helpers'
+
+export async function sendPostDeletion(action: `HIDE`, postCID: string, authorID: string) {
+	const event = {
+		action,
+		postCID,
+		timestamp: Date.now(),
+		authorID,
+	}
+
+	const signature = signContent(event, authorID)
+
+	if (!signature) {
+		throw new Error(`Post deletion signing failed`)
+	}
+
+	await axios.post(`${capsuleOrbit}/posts`, {
+		event,
+		sig: uint8ArrayToHexString(signature),
+	})
+}
