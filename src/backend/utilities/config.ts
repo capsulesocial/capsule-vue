0976d9ff2565a7daa50349bcfec56ff7c04a0fd9
Christos Panagiotakopoulos <chrispanag@gmail.com>
2021-11-02 13:25:59 +0200

    [feat] remove unused capsule-server env variable

--- src/backend/utilities/config.ts
+++ src/backend/utilities/config.ts
@@ -1,4 +1,3 @@
-export const capsuleServer = process.env.SERVER_URL || `https://test-node.capsule.social/server`
 export const capsuleOrbit = process.env.ORBIT_URL || `https://test-node.capsule.social/orbit`
 export const domain = process.env.DOMAIN || `http://localhost:3000`
 export const contractName = process.env.CONTRACT_NAME || `capsule-vue`
