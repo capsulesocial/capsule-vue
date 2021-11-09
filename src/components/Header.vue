39f14738e05db2a175a94457a5e63cb741ef9d8d
jack dishman <me@jackdishman.com>
2021-11-09 14:01:50 -0800

    Top right dropdown on click of avatar

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -11,8 +11,40 @@
 			<BrandedButton :text="`Write a Post`" :action="togglePostEditor" class="mx-4" />
 		</nav>
 		<!-- Right side: icons and avatar -->
-		<div class="flex flex-row">
-			<Avatar :avatar="avatar" :authorID="$store.state.session.id" />
+		<div class="flex flex-row relative">
+			<button class="dropdown focus:outline-none" @click="showDropdown = !showDropdown">
+				<Avatar class="dropdown" :avatar="avatar" :authorID="$store.state.session.id" :noClick="true" />
+			</button>
+			<!-- Dropdown: Profile, settings, disconnect -->
+			<div
+				v-show="showDropdown"
+				class="
+					absolute
+					flex flex-col
+					mt-12
+					-ml-20
+					rounded-lg
+					shadow-lg
+					p-3
+					bg-gradient-to-r
+					from-lightBGStart
+					to-lightBGStop
+					backdrop-filter backdrop-blur-lg
+				"
+			>
+				<nuxt-link :to="$store.state.session.id" class="text-left w-full flex flex-row items-center text-gray7"
+					><ProfileIcon class="flex-shrink-0 w-5 h-5 mr-2 mb-2" />Profile</nuxt-link
+				>
+				<nuxt-link to="/settings" class="text-left w-full flex flex-row items-center text-gray7">
+					<SettingsIcon class="flex-shrink-0 w-5 h-5 mr-2 mb-2" />Settings</nuxt-link
+				>
+				<button
+					class="focus:outline-none w-full text-left flex flex-row items-center text-lightError"
+					@click="disconnect"
+				>
+					<LogoutIcon class="flex-shrink-0 w-5 h-5 mr-2" />Disconnect
+				</button>
+			</div>
 		</div>
 	</div>
 </template>
@@ -25,14 +57,24 @@ import { keyStores } from 'near-api-js'
 import CapsuleIcon from '@/components/icons/Capsule.vue'
 import Avatar from '@/components/Avatar.vue'
 import BrandedButton from '@/components/BrandedButton.vue'
+import ProfileIcon from '@/components/icons/Profile.vue'
+import SettingsIcon from '@/components/icons/Settings.vue'
+import LogoutIcon from '@/components/icons/Logout.vue'
 
 import { MutationType, namespace as sessionStoreNamespace } from '~/store/session'
+
+interface IData {
+	showDropdown: boolean
+}
 
 export default Vue.extend({
 	components: {
 		CapsuleIcon,
 		Avatar,
 		BrandedButton,
+		ProfileIcon,
+		SettingsIcon,
+		LogoutIcon,
 	},
 	props: {
 		avatar: {
@@ -40,14 +82,34 @@ export default Vue.extend({
 			default: ``,
 		},
 	},
-	data() {
-		return {}
+	data(): IData {
+		return {
+			showDropdown: false,
+		}
 	},
-	mounted() {},
+	created() {
+		// Set filter dropdown event handler
+		window.addEventListener(`click`, this.handleDropdown, false)
+	},
+	destroyed() {
+		window.removeEventListener(`click`, this.handleDropdown)
+	},
 	methods: {
 		...mapMutations(sessionStoreNamespace, {
 			endSession: MutationType.LOGOUT,
 		}),
+		disconnect(): void {
+			this.endSession()
+			this.$router.push(`/`)
+		},
+		handleDropdown(e: any): void {
+			if (!e.target || e.target.parentNode === null || e.target.parentNode.classList === undefined) {
+				return
+			}
+			if (!e.target.parentNode.classList.contains(`dropdown`)) {
+				this.showDropdown = false
+			}
+		},
 		getStyles(tab: string): string {
 			let res: string = ``
 			// Check if current tab
