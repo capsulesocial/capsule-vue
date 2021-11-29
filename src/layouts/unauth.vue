5efe33893e373e2899988e0d5c0f55dc4b61b73d
ldmweb <lilian.desvaux98@gmail.com>
2021-11-29 19:27:43 +0100

    polish login/signup styling

--- src/layouts/unauth.vue
+++ src/layouts/unauth.vue
@@ -1,6 +1,6 @@
 <template>
 	<main
-		class="emoji-cursor h-screen p-0 m-0 bg-img-unauth"
+		class="h-screen p-0 m-0 bg-img-unauth"
 		:style="{ backgroundImage: `url(${require(`@/assets/images/brand/auth.jpg`)})` }"
 	>
 		<Nuxt />
@@ -14,10 +14,6 @@ export default Vue.extend({})
 </script>
 
 <style>
-.emoji-cursor {
-	cursor: url("data:image/svg+xml;utf8,<svg xmlns='http://www.w3.org/2000/svg'  width='40' height='48' viewport='0 0 100 100' style='fill:black;font-size:24px;'><text y='50%'>🚀</text></svg>"),
-		auto;
-}
 .bg-img-unauth {
 	background-position: right top;
 	background-size: cover;
