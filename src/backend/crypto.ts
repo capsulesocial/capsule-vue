36942597d9f344694c112d60e7ea5f542c827fb9
Ashutosh Ahelleya <ashutosh.ahelleya@gmail.com>
2021-12-01 20:42:14 +0530

    Implement keygen, encryption, decryption of data (#228)
    * Implement keygen, encryption, decryption
    
    * Update encryption and decryption functions
    
    Encrypt only content field of a post
    
    * Stabilise post object before verification

---
+++ src/backend/crypto.ts
@@ -0,0 +1,65 @@
+import { hexStringToUint8Array, uint8ArrayToHexString } from './utilities/helpers'
+import { Post } from './post'
+import { signContent, verifyContent } from './utilities/keys'
+import { getUserInfoNEAR } from './near'
+
+async function _encryptData(data: Uint8Array, counter: Uint8Array, key: Uint8Array) {
+	const derivedKey = await window.crypto.subtle.importKey(`raw`, key, { name: `AES-CTR` }, false, [
+		`encrypt`,
+		`decrypt`,
+	])
+	const encryptedData = await window.crypto.subtle.encrypt({ name: `AES-CTR`, counter, length: 64 }, derivedKey, data)
+	return new Uint8Array(encryptedData)
+}
+
+async function _decryptData(data: Uint8Array, counter: Uint8Array, key: Uint8Array) {
+	const derivedKey = await window.crypto.subtle.importKey(`raw`, key, { name: `AES-CTR` }, false, [
+		`encrypt`,
+		`decrypt`,
+	])
+	const decryptedData = await window.crypto.subtle.decrypt({ name: `AES-CTR`, counter, length: 64 }, derivedKey, data)
+	return new Uint8Array(decryptedData)
+}
+
+export async function encryptAndSignData(data: Post) {
+	// 32-byte key for AES-256
+	const key = window.crypto.getRandomValues(new Uint8Array(32))
+	// 16-byte counter block for AES-256
+	const counter = window.crypto.getRandomValues(new Uint8Array(16))
+
+	const ec = new TextEncoder()
+	const byteData = ec.encode(data.content)
+
+	const encryptedData = await _encryptData(byteData, counter, key)
+	data.content = uint8ArrayToHexString(encryptedData)
+
+	const signature = await signContent(data)
+	if (!signature) {
+		throw new Error(`Data signing failed!`)
+	}
+
+	return {
+		post: data,
+		key: uint8ArrayToHexString(key),
+		counter: uint8ArrayToHexString(counter),
+		signature: uint8ArrayToHexString(signature),
+	}
+}
+
+export async function verifyAndDecryptData(data: Post, key: string, counter: string, signature: string) {
+	const { publicKey } = await getUserInfoNEAR(data.authorID)
+	const verified = verifyContent(data, hexStringToUint8Array(signature), publicKey)
+	if (!verified) {
+		throw new Error(`Signature not verified!`)
+	}
+
+	const dec = new TextDecoder()
+
+	const decryptedData = await _decryptData(
+		hexStringToUint8Array(data.content),
+		hexStringToUint8Array(counter),
+		hexStringToUint8Array(key),
+	)
+	data.content = dec.decode(decryptedData)
+	return data
+}
