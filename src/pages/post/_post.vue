409818281812e3b40d03715ca8e29bdbf12d7bc3
ldmweb <lilian.desvaux98@gmail.com>
2021-12-03 12:50:31 +0100

    add loader + error handling toast

--- src/pages/post/_post.vue
+++ src/pages/post/_post.vue
@@ -139,6 +139,7 @@
 		<article v-show="isLoading" class="flex justify-center w-full mt-20">
 			<div class="loader m-5"></div>
 		</article>
+<<<<<<< HEAD
 		<!-- Show Post preview card on quote repost -->
 		<div v-if="showQuoteRepost">
 			<PostCard
@@ -154,6 +155,8 @@
 				@closePopup="closePopup"
 			/>
 		</div>
+=======
+>>>>>>> 5fe647d (add loader + error handling toast)
 	</div>
 </template>
 
@@ -193,9 +196,12 @@ interface IData {
 	repostCount: number
 	comments: ICommentData[]
 	isLoading: boolean
+<<<<<<< HEAD
 	showQuoteRepost: boolean
 	following: Set<string>
 	bookmarksCount: number
+=======
+>>>>>>> 5fe647d (add loader + error handling toast)
 }
 
 export default Vue.extend({
@@ -229,9 +235,12 @@ export default Vue.extend({
 			repostCount: -1,
 			comments: [],
 			isLoading: true,
+<<<<<<< HEAD
 			showQuoteRepost: false,
 			following: new Set(),
 			bookmarksCount: -1,
+=======
+>>>>>>> 5fe647d (add loader + error handling toast)
 		}
 	},
 	async created() {
