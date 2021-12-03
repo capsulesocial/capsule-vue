a956d17a148c80136a941a4ce24c30527c28b63f
ldmweb <lilian.desvaux98@gmail.com>
2021-12-03 12:50:31 +0100

    add loader + error handling toast

--- src/pages/post/_post.vue
+++ src/pages/post/_post.vue
@@ -1,7 +1,7 @@
 <template>
-	<div v-if="post && author" id="post" class="w-full flex justify-center">
+	<div id="post" class="w-full flex justify-center">
 		<!-- Inner post area -->
-		<div style="width: 760px; max-width: 760px; height: fit-content">
+		<div v-if="post && author" style="width: 760px; max-width: 760px; height: fit-content">
 			<!-- Magic header that disappears on scroll down -->
 			<header
 				id="header"
@@ -233,6 +233,7 @@ export default Vue.extend({
 		// Fetch post from IPFS,
 		this.post = await getRegularPost(this.$route.params.post)
 		if (this.post === null) {
+			this.$toastError(`This post has not been found`)
 			throw new Error(`Post is null!`)
 		}
 		const postMetadata = await getOnePost(this.$route.params.post, this.$store.state.session.id)
