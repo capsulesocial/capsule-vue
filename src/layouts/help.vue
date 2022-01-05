aab575e5825f8e6683118fae1c6a07487c328ae6
ldmweb <lilian.desvaux98@gmail.com>
2022-01-05 20:09:28 +0100

    implement help page

---
+++ src/layouts/help.vue
@@ -0,0 +1,134 @@
+<template>
+	<main class="h-screen p-0 m-0 bg-img" :style="{ backgroundImage: `url(` + $store.state.backgroundImage + `)` }">
+		<!-- Wrapper -->
+		<div class="w-full flex justify-center">
+			<div class="flex flex-col" style="width: 1220px">
+				<!-- Header -->
+				<header class="w-full sticky top-0 py-5 bg-gradient-to-r z-10 pb-4">
+					<Header :avatar="avatar" />
+				</header>
+				<!-- Body -->
+				<div>
+					<!-- Content -->
+					<section class="flex flex-row relative">
+						<nuxt-child
+							style="
+								width: 750px;
+								min-height: calc(100vh - 70px);
+								height: calc(100vh - 70px);
+								backdrop-filter: blur(10px);
+							"
+							class="fixed overflow-y-auto rounded-lg shadow-lg mr-5 bg-gradient-to-r from-lightBGStart to-lightBGStop modal-animation"
+							:class="showWidgets ? `` : `z-10`"
+							:toggleFriend="toggleFriend"
+							:following="following"
+							:followers="followers"
+							:userIsFollowed="userIsFollowed"
+						/>
+						<!-- Widgets -->
+						<aside
+							class="fixed overflow-y-auto p-4 -mt-4 modal-animation"
+							:class="showWidgets ? `z-10` : ``"
+							style="margin-left: 755px; width: 485px; min-height: calc(100vh - 150px); height: calc(100vh - 150px)"
+						>
+							<SupportWidget />
+							<Footer />
+						</aside>
+					</section>
+				</div>
+			</div>
+		</div>
+		<!-- Onboarding Wizard -->
+		<OnboardingWizard v-if="$store.state.recentlyJoined" />
+	</main>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import Header from '@/components/Header.vue'
+import Footer from '@/components/Footer.vue'
+import SupportWidget from '@/components/widgets/Support.vue'
+import OnboardingWizard from '@/components/OnboardingWizard.vue'
+
+import { getProfile, Profile } from '@/backend/profile'
+import { getPhotoFromIPFS } from '@/backend/photos'
+import { followChange, getFollowersAndFollowing } from '@/backend/following'
+
+interface IData {
+	profile: Profile | null
+	avatar: string | ArrayBuffer | null
+	showWidgets: boolean
+	following: Set<string>
+	followers: Set<string>
+	userIsFollowed: boolean
+}
+
+export default Vue.extend({
+	components: {
+		Header,
+		Footer,
+		SupportWidget,
+		OnboardingWizard,
+	},
+	data(): IData {
+		return {
+			profile: null,
+			avatar: null,
+			showWidgets: false,
+			following: new Set(),
+			followers: new Set(),
+			userIsFollowed: false,
+		}
+	},
+	async created() {
+		// Check if logged in user
+		if (this.$store.state.session.id === ``) {
+			this.$router.push(`/`)
+		}
+		// get logged in profile
+		const { profile } = await getProfile(this.$store.state.session.id)
+		this.profile = profile
+		// Get avatar
+		if (this.profile && this.profile.avatar.length > 1) {
+			getPhotoFromIPFS(this.profile.avatar).then((p) => {
+				this.avatar = p
+			})
+		}
+		// Get followers and following
+		const { followers, following } = await getFollowersAndFollowing(this.$store.state.session.id)
+		this.following = following
+		this.followers = followers
+		this.userIsFollowed = followers.has(this.$store.state.session.id)
+	},
+	methods: {
+		async toggleFriend(authorID: string) {
+			if (authorID !== this.$store.state.session.id) {
+				await followChange(this.following.has(authorID) ? `UNFOLLOW` : `FOLLOW`, this.$store.state.session.id, authorID)
+				const data = await getFollowersAndFollowing(this.$store.state.session.id, true)
+				this.following = data.following
+			}
+		},
+		async updateFollowers() {
+			const { followers, following } = await getFollowersAndFollowing(this.$store.state.session.id, true)
+			this.followers = followers
+			this.following = following
+		},
+		toggleZIndex() {
+			this.showWidgets = !this.showWidgets
+		},
+		saveDraftState(): void {
+			if (this.$refs.editor) {
+				// @ts-ignore
+				this.$refs.editor.saveContent()
+			}
+		},
+	},
+})
+</script>
+
+<style>
+.bg-img {
+	background-attachment: fixed;
+	background-size: cover;
+}
+</style>
--- src/pages/help/index.vue
+++ src/pages/help/index.vue
@@ -1,27 +1,185 @@
 <template>
-	<main class="border-l border-r" style="width: 750px; height: calc(100vh - 95px)">
-		<section>
-			<img src="~/assets/images/brand/help-page.webp" alt="" class="w-full" />
-		</section>
+	<main style="width: 750px; height: calc(100vh - 95px)">
 		<section>
 			<!-- Page title -->
-			<article class="px-5">
-				<h2 class="text-3xl font-sans font-semibold">Need Some Help?</h2>
+			<article class="px-6 py-5">
+				<h1 class="font-semibold text-primary" style="font-size: 2.6rem">Need Some Help?</h1>
 			</article>
-			<article class="mt-5 p-10" style="background-color: #f9faff">
-				<h6 class="text-lg text-lightPrimary pb-5">Using Capsule Social</h6>
-				<h6 class="text-lg text-lightPrimary pb-5">Manage My Account</h6>
-				<h6 class="text-lg text-lightPrimary pb-5">Fix a problem</h6>
-				<h6 class="text-lg text-lightPrimary pb-5">Policies and Reporting</h6>
-				<h6 class="text-lg text-lightPrimary">Chat with Support</h6>
-			</article>
+			<div class="container px-6">
+				<h3 class="text-primary text-xl font-semibold mb-6 mt-4">Frequently Asked Questions</h3>
+				<article class="message w-10/12 mb-4">
+					<div class="message-header flex justify-between items-center relative" @click="toggleAccordion1">
+						<h4 class="text-lightPrimaryText text-base font-semibold">Are my personal data used by Capsule ?</h4>
+						<Plus :class="isOpen1 ? `plus0` : `plus1`" class="icon fill-current w-5 h-5 absolute right-0" />
+						<Minus :class="!isOpen1 ? `minus0` : `minus1`" class="icon fill-current w-5 h-5 absolute right-0" />
+					</div>
+					<div :class="isOpen1 ? `` : `is-closed`" class="message-body w-10/12">
+						<div class="message-content py-2 text-gray5 text-sm">
+							Capsule is a decentralized discourse platform. We don’t collect any data of any kind from you and respect
+							our users values.
+						</div>
+					</div>
+				</article>
+				<article class="message w-10/12 mb-4">
+					<div class="message-header flex justify-between items-center relative" @click="toggleAccordion2">
+						<h4 class="text-lightPrimaryText text-base font-semibold">Do I need a crypto-wallet to use Capsule ?</h4>
+						<Plus :class="isOpen2 ? `plus0` : `plus1`" class="icon fill-current w-5 h-5 absolute right-0" />
+						<Minus :class="!isOpen2 ? `minus0` : `minus1`" class="icon fill-current w-5 h-5 absolute right-0" />
+					</div>
+					<div :class="isOpen2 ? `` : `is-closed`" class="message-body w-10/12">
+						<div class="message-content py-2 text-gray5 text-sm">
+							Capsule is a decentralized discourse platform. We don’t collect any data of any kind from you and respect
+							our users values.
+						</div>
+					</div>
+				</article>
+				<article class="message w-10/12 mb-4">
+					<div class="message-header flex justify-between items-center relative" @click="toggleAccordion3">
+						<h4 class="text-lightPrimaryText text-base font-semibold">Can I post on Capsule for free ?</h4>
+						<Plus :class="isOpen3 ? `plus0` : `plus1`" class="icon fill-current w-5 h-5 absolute right-0" />
+						<Minus :class="!isOpen3 ? `minus0` : `minus1`" class="icon fill-current w-5 h-5 absolute right-0" />
+					</div>
+					<div :class="isOpen3 ? `` : `is-closed`" class="message-body w-10/12">
+						<div class="message-content py-2 text-gray5 text-sm">
+							Capsule is a decentralized discourse platform. We don’t collect any data of any kind from you and respect
+							our users values.
+						</div>
+					</div>
+				</article>
+				<article class="message w-10/12 mb-4">
+					<div class="message-header flex justify-between items-center relative" @click="toggleAccordion4">
+						<h4 class="text-lightPrimaryText text-base font-semibold">
+							Can I share my Capsule content on other platforms ?
+						</h4>
+						<Plus :class="isOpen4 ? `plus0` : `plus1`" class="icon fill-current w-5 h-5 absolute right-0" />
+						<Minus :class="!isOpen4 ? `minus0` : `minus1`" class="icon fill-current w-5 h-5 absolute right-0" />
+					</div>
+					<div :class="isOpen4 ? `` : `is-closed`" class="message-body w-10/12">
+						<div class="message-content py-2 text-gray5 text-sm">
+							Capsule is a decentralized discourse platform. We don’t collect any data of any kind from you and respect
+							our users values.
+						</div>
+					</div>
+				</article>
+				<article class="message w-10/12 mb-4">
+					<div class="message-header flex justify-between items-center relative" @click="toggleAccordion5">
+						<h4 class="text-lightPrimaryText text-base font-semibold">How can I delete my Capsule account ?</h4>
+						<Plus :class="isOpen5 ? `plus0` : `plus1`" class="icon fill-current w-5 h-5 absolute right-0" />
+						<Minus :class="!isOpen5 ? `minus0` : `minus1`" class="icon fill-current w-5 h-5 absolute right-0" />
+					</div>
+					<div :class="isOpen5 ? `` : `is-closed`" class="message-body w-10/12">
+						<div class="message-content py-2 text-gray5 text-sm">
+							Capsule is a decentralized discourse platform. We don’t collect any data of any kind from you and respect
+							our users values.
+						</div>
+					</div>
+				</article>
+				<article class="message w-10/12 mb-4">
+					<div class="message-header flex justify-between items-center relative" @click="toggleAccordion6">
+						<h4 class="text-lightPrimaryText text-base font-semibold">
+							How to Signal inappropriate Content on Capsule ?
+						</h4>
+						<Plus :class="isOpen6 ? `plus0` : `plus1`" class="icon fill-current w-5 h-5 absolute right-0" />
+						<Minus :class="!isOpen6 ? `minus0` : `minus1`" class="icon fill-current w-5 h-5 absolute right-0" />
+					</div>
+					<div :class="isOpen6 ? `` : `is-closed`" class="message-body w-10/12">
+						<div class="message-content py-2 text-gray5 text-sm">
+							Capsule is a decentralized discourse platform. We don’t collect any data of any kind from you and respect
+							our users values.
+						</div>
+					</div>
+				</article>
+			</div>
 		</section>
 	</main>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
+
+import Plus from '@/components/icons/Plus.vue'
+import Minus from '@/components/icons/Minus.vue'
+
+interface IData {
+	isOpen1: boolean
+	isOpen2: boolean
+	isOpen3: boolean
+	isOpen4: boolean
+	isOpen5: boolean
+	isOpen6: boolean
+}
+
 export default Vue.extend({
-	layout: `home`,
+	components: {
+		Plus,
+		Minus,
+	},
+	layout: `help`,
+	data(): IData {
+		return {
+			isOpen1: true,
+			isOpen2: false,
+			isOpen3: false,
+			isOpen4: false,
+			isOpen5: false,
+			isOpen6: false,
+		}
+	},
+	methods: {
+		toggleAccordion1() {
+			this.isOpen1 = !this.isOpen1
+		},
+		toggleAccordion2() {
+			this.isOpen2 = !this.isOpen2
+		},
+		toggleAccordion3() {
+			this.isOpen3 = !this.isOpen3
+		},
+		toggleAccordion4() {
+			this.isOpen4 = !this.isOpen4
+		},
+		toggleAccordion5() {
+			this.isOpen5 = !this.isOpen5
+		},
+		toggleAccordion6() {
+			this.isOpen6 = !this.isOpen6
+		},
+	},
 })
 </script>
+<style>
+.message-header {
+	cursor: pointer;
+}
+
+.message-header .icon {
+	transition: all 0.6s;
+}
+
+.message-body {
+	padding: 0;
+	max-height: 10em;
+	overflow: hidden;
+	transition: all 0.6s;
+}
+
+.plus0 {
+	opacity: 0;
+}
+
+.plus1 {
+	opacity: 1;
+}
+
+.minus0 {
+	opacity: 0;
+}
+
+.minus1 {
+	opacity: 1;
+}
+
+.is-closed {
+	max-height: 0;
+}
+</style>
