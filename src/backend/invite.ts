678acc59a29e498d4c11ef2355f0b9d22fdedb8e
ashutosh1206 <ashutosh.ahelleya@gmail.com>
2022-01-11 13:16:13 +0530

    Generate invite codes and get remaining invites

--- src/backend/invite.ts
+++ src/backend/invite.ts
@@ -1,5 +1,7 @@
 import axios from 'axios'
-import { capsuleServer } from './utilities/config'
+import { capsuleServer, sigValidity } from './utilities/config'
+import { uint8ArrayToHexString } from './utilities/helpers'
+import { signContent } from './utilities/keys'
 
 export async function verifyCodeAndGetToken(inviteCode: string) {
 	const response = await axios.post(`${capsuleServer}/invite/verify`, { code: inviteCode })
@@ -19,6 +21,22 @@ export async function verifyTokenAndOnboard(accountId: string) {
 	)
 	removeToken()
 	return response.data.data as string
+}
+
+export async function generateInviteCode(inviter: string): Promise<{ inviteCode: string; invitesRemaining: number }> {
+	const exp = Date.now() + sigValidity
+	const signature = await signContent({ inviter, exp })
+	if (!signature) {
+		throw new Error(`Object signing failed`)
+	}
+	const sig = uint8ArrayToHexString(signature)
+	const response = await axios.get(`${capsuleServer}/invite`, { params: { exp, sig, inviter } })
+	return response.data.data
+}
+
+export async function getInvitesRemaining(inviter: string): Promise<number> {
+	const response = await axios.get(`${capsuleServer}/invite/remaining`, { params: { inviter } })
+	return response.data.data
 }
 
 function setToken(token: string) {
