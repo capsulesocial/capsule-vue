001b877c07bf08bc53575413f0d33e3dca1382a3
Christos Panagiotakopoulos <chrispanag@gmail.com>
2022-01-12 13:43:48 +0200

    [feat] add bookmarks/:user route to retrieve all bookmarks of a user

--- src/backend/bookmarks.ts
+++ src/backend/bookmarks.ts
@@ -2,6 +2,7 @@ import axios from 'axios'
 import { signContent } from './utilities/keys'
 import { nodeUrl } from './utilities/config'
 import { uint8ArrayToHexString } from './utilities/helpers'
+import { IPostResponse } from './post'
 
 export async function sendBookmarkEvent(action: `ADD` | `REMOVE`, authorID: string, postCID: string) {
 	const event = { action, authorID, postCID, timestamp: Date.now() }
@@ -18,4 +19,16 @@ export async function isPostBookmarkedByUser(postCID: string, userID: string): P
 	const response = await axios.get(`${nodeUrl()}/bookmark/${userID}/${postCID}`)
 
 	return response.data.data.isBookmarked
+}
+
+export async function getBookmarksOfUser(userID: string, category?: string): Promise<IPostResponse[]> {
+	const response = await axios.get(`${nodeUrl()}/bookmark/${userID}`)
+
+	const bookmarks: IPostResponse[] = response.data.data.bookmarks
+
+	if (category) {
+		return bookmarks.filter((p) => p.post.category === category)
+	}
+
+	return bookmarks
 }
