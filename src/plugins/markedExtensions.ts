3a80b7069504720acdce9187f4bb4877ecf08720
Rahul Trivedi <rahultrivedi180@gmail.com>
2022-01-13 19:23:34 +0100

    Ipfs image extension issue

--- src/plugins/markedExtensions.ts
+++ src/plugins/markedExtensions.ts
@@ -10,3 +10,28 @@ export const markedRenderer: marked.RendererObject = {
 		return `<img src="${href}" alt="${title}" alt="${text}">`
 	},
 }
+
+export const ipfsImgExtension = {
+	name: `ipfsImg`,
+	level: `inline`,
+	start(src: string) {
+		return src.match(/\\\[ipfs\\_img/)?.index
+	},
+	// eslint-disable-next-line consistent-return
+	tokenizer(src: string) {
+		const ipfsImgRule = /\\\[ipfs\\_img [^>]*cid="[^"]*"\\]/
+		const ipfsCidRule = /cid="([A-Za-z0-9]*)"/
+		const ipfsImgMatch = ipfsImgRule.exec(src)
+		const ipfsCidMatch = ipfsCidRule.exec(src)
+		if (ipfsImgMatch && ipfsCidMatch) {
+			return {
+				type: `ipfsImg`,
+				raw: ipfsImgMatch[0],
+				cid: ipfsCidMatch[1],
+			}
+		}
+	},
+	renderer(token: any) {
+		return `<img alt="${token.cid}" class="ipfs_img" />`
+	},
+}
