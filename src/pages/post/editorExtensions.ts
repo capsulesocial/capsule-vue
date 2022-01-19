e59abeba5264daeed271fcbb3c6cb9d7a73953f1
Christos Panagiotakopoulos <chrispanag@gmail.com>
2022-01-19 12:49:46 +0200

    fix up previous commit

---
+++ src/pages/post/editorExtensions.ts
@@ -0,0 +1,90 @@
+import Quill from 'quill'
+import TurndownService from 'turndown'
+import hljs from 'highlight.js'
+import { marked } from 'marked'
+
+// Marked
+
+export const markedRenderer: marked.RendererObject = {
+	code(src: string) {
+		const highlightedCode = hljs.highlightAuto(src).value
+		return `<pre><code class="hljs">${highlightedCode}</code></pre>`
+	},
+}
+
+// Quill
+
+const BlockEmbed = Quill.import(`blots/block/embed`)
+
+export class ImageBlot extends BlockEmbed {
+	static blotName = `image`
+	static tagName = `img`
+
+	static create(value: { alt: string; url: string }) {
+		const node = super.create()
+		node.setAttribute(`alt`, value.alt)
+		node.setAttribute(`src`, value.url)
+		return node
+	}
+
+	static value(node: { getAttribute: (arg0: string) => any }) {
+		return {
+			alt: node.getAttribute(`alt`),
+			url: node.getAttribute(`src`),
+		}
+	}
+}
+
+// Turndown
+
+export const preRule: TurndownService.Rule = {
+	filter: [`pre`],
+	replacement: (_, node) => {
+		// eslint-disable-next-line quotes
+		return '```\n' + node.textContent + '```'
+	},
+}
+
+export const ipfsImageRule: TurndownService.Rule = {
+	filter: [`img`],
+	replacement: (_, node) => {
+		if (`getAttribute` in node) {
+			return `<ipfsimage alt="${node.getAttribute(`alt`)}" class="ipfs_img" cid="${node.getAttribute(
+				`alt`,
+			)}"></ipfsimage>`
+		}
+
+		throw new Error(`getAttributes does not exist on node`)
+	},
+}
+
+// Misc
+
+export const imgRegexp = (cid: string) =>
+	new RegExp(
+		`<ipfsimage ((alt="${cid}")|(cid="${cid}")|(class="ipfs_img"))\\s+((alt="${cid}")|(class="ipfs_img")|(cid="${cid}"))\\s+((alt="${cid}")|(class="ipfs_img")|(cid="${cid}"))></ipfsimage>`,
+		`g`,
+	)
+
+export function transformPostToTemplate(body: string, postImages?: Array<string>) {
+	if (!postImages) {
+		return body
+	}
+	for (const p of postImages) {
+		body = body.replace(imgRegexp(p), `<IpfsImage alt="${p}" class="ipfs_img" :cid="'${p}'" />`)
+	}
+	return body
+}
+
+export function createPostImagesArray(content: string, uploadedImages: Set<string>) {
+	const usedImages: string[] = []
+	uploadedImages.forEach((cid) => {
+		if (!content.match(imgRegexp(cid))) {
+			return
+		}
+
+		usedImages.push(cid)
+	})
+
+	return usedImages
+}
