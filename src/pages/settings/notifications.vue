3e365e9148bcc7f809be6c54a1878d78d84f2810
jack dishman <me@jackdishman.com>
2022-01-28 15:37:23 -0800

    populated notifications settings page

--- src/pages/settings/notifications.vue
+++ src/pages/settings/notifications.vue
@@ -1,5 +1,14 @@
 <template>
-	<main>Notifications settings page</main>
+	<main>
+		<!-- Mobile back button -->
+		<nuxt-link to="/settings" class="mb-4 flex items-center xl:hidden">
+			<span class="bg-gray1 mr-2 rounded-full p-1"><ChevronLeft /></span>
+			<h6 class="font-semibold">All Settings</h6>
+		</nuxt-link>
+		<!-- Email notifications -->
+		<h2 class="text-primary mb-4 text-sm font-semibold">Email notifications</h2>
+		<p class="text-gray5">You are currently receiving email notifications from the following authors:</p>
+	</main>
 </template>
 
 <script lang="ts">
