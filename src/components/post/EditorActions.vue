991547edc60b217c3c77744d5768f1892edf3975
ldmweb <lilian.desvaux98@gmail.com>
2022-02-02 20:08:13 +0100

    create add Content button to add images on a new line + adjust some styling

---
+++ src/components/post/EditorActions.vue
@@ -0,0 +1,90 @@
+<template>
+	<div class="flex items-end">
+		<button
+			class="focus:outline-none hover:text-primary toggle flex items-end"
+			:class="showSocialShares ? 'text-primary' : ''"
+			@click.stop="toggleDropdown"
+		>
+			<div class="text-gray3 hover:text-primary hover:fill-primary flex">
+				<PlusIcon :isActive="showSocialShares" style="transform: scale(1.2); margin-top: -1px" />
+			</div>
+		</button>
+		<div
+			v-show="showSocialShares"
+			:class="
+				$store.state.settings.darkMode
+					? 'bg-lightBG text-lightPrimaryText border-lightBorder'
+					: 'bg-darkBG text-darkPrimaryText border-darkBorder'
+			"
+			class="border-lightBorder modal-animation dropdownActionsOpen absolute z-10 flex flex-col rounded-lg border p-3 shadow-lg"
+			style="left: 50px; top: -12px"
+		>
+			<!-- Add Image -->
+			<button
+				:class="$store.state.settings.darkMode ? 'hover:text-lightPrimaryText' : 'hover:text-darkActive'"
+				class="focus:outline-none text-gray5 flex"
+				@click="$emit(`image`)"
+			>
+				<ImageIcon class="h-5 w-5 fill-current" />
+			</button>
+		</div>
+	</div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import PlusIcon from '@/components/icons/Plus.vue'
+import ImageIcon from '@/components/icons/Image.vue'
+
+interface IData {
+	showSocialShares: boolean
+}
+
+export default Vue.extend({
+	components: {
+		PlusIcon,
+		ImageIcon,
+	},
+	data(): IData {
+		return {
+			showSocialShares: false,
+		}
+	},
+	created() {
+		window.addEventListener(
+			`click`,
+			(e: any): void => {
+				if (!e.target) {
+					return
+				}
+				if (
+					e.target.parentNode === null ||
+					e.target.parentNode.classList === undefined ||
+					!e.target.parentNode.classList.contains(`toggle`)
+				) {
+					this.showSocialShares = false
+				}
+			},
+			false,
+		)
+	},
+	methods: {
+		toggleDropdown() {
+			this.showSocialShares = !this.showSocialShares
+		},
+	},
+})
+</script>
+<style>
+.dropdownActionsOpen::before {
+	content: '';
+	position: absolute;
+	top: 0.85rem;
+	left: -0.5rem;
+	transform: rotate(45deg);
+	width: 1rem;
+	height: 1rem;
+	background-color: #fff;
+	border-radius: 2px;
+}
+</style>
