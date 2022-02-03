5a5590f0d068d01f1d3a995d3bda9603210f8a9b
jack dishman <me@jackdishman.com>
2022-02-02 19:27:44 -0800

    self-view subscription notifications template

--- src/components/popups/ConfigureNotifications.vue
+++ src/components/popups/ConfigureNotifications.vue
@@ -11,54 +11,91 @@
 				<h2 class="text-primary text-xl font-semibold">Configure Notifications</h2>
 				<button class="bg-gray1 focus:outline-none rounded-full p-1" @click="closePopup"><CloseIcon /></button>
 			</div>
-			<!-- Avatar and description -->
-			<div class="flex mb-6">
-				<Avatar :avatar="avatar" :authorID="profile.id" :noClick="true" :size="`w-12 h-12`" />
-				<p class="text-gray7 ml-4">
-					Subscribe to {{ profile.name }}'s email notifications and manage existing subscriptions here
+			<!-- Self-view view -->
+			<div v-if="$store.state.session.id === $route.params.id">
+				<p class="text-gray7">
+					Display highlighted tags on your profile for readers to enable email notifications to receive your posts. You
+					can display up to three different tags:
 				</p>
-			</div>
-			<!-- Topics -->
-			<div class="grid grid-cols-5 mb-3">
-				<h6 class="col-span-1 font-semibold text-lg">Topics</h6>
-				<div class="col-span-4 flex justify-between items-center">
-					<p>Receive email notifications for all new posts</p>
-					<label class="switch">
-						<input type="checkbox" />
-						<span class="slider round"></span>
-					</label>
-				</div>
-			</div>
-			<!-- Show list of available tags to subscribe to -->
-			<div class="grid grid-cols-5 mb-2">
-				<span class="col-span-1"></span>
-				<p class="col-span-4">Receive notifications based on tag:</p>
-			</div>
-			<div class="grid grid-cols-5 mb-6">
-				<span class="col-span-1"></span>
-				<div class="flex flex-row flex-wrap col-span-4">
-					<TagCard :tag="`placeholder1`" :noClick="true" class="mr-2 mb-2" />
-					<TagCard :tag="`placeholder2`" :noClick="true" class="mr-2 mb-2" />
-					<TagCard :tag="`placeholder3`" :noClick="true" class="mr-2 mb-2" />
-				</div>
-			</div>
-			<!-- Select email -->
-			<div class="grid grid-cols-5 mb-6">
-				<h6 class="col-span-1 font-semibold text-lg">Destination</h6>
-				<div class="col-span-4 flex flex-col">
-					<!-- For each active email -->
-					<div v-if="$store.state.session.email" class="flex flex-row items-center">
-						<input type="radio" checked />
-						<p class="ml-2">{{ $store.state.session.email }}</p>
+				<div class="grid grid-cols-5 mb-3">
+					<!-- Add tag input -->
+					<label for="tagInput" class="font-semibold col-span-1 mt-4">Tags</label>
+					<div class="col-span-4 bg-gray1 rounded-lg px-4 py-2 my-4">
+						<input
+							id="tagInput"
+							:ref="tagInput"
+							type="text"
+							placeholder="Add more specific topics to be notified on..."
+							class="w-full text-gray7 bg-gray1 focus:outline-none"
+						/>
+					</div>
+					<div class="col-span-1"></div>
+					<div class="flex flex-row flex-wrap col-span-4">
+						<button class="bg-gray2 rounded-lg flex items-center text-gray7 px-4 py-2 focus:outline-none mr-6">
+							placeholder1
+							<CloseIcon class="p-1" />
+						</button>
+						<button class="bg-gray2 rounded-lg flex items-center text-gray7 px-4 py-2 focus:outline-none">
+							placeholder1
+							<CloseIcon class="p-1" />
+						</button>
+					</div>
+					<div class="col-span-5 flex justify-end mt-4">
+						<BrandedButton :text="`Save changes`" />
 					</div>
 				</div>
 			</div>
-			<!-- Submit or manage subscriptions -->
-			<div class="flex justify-end items-center">
-				<nuxt-link to="/settings/notifications" class="text-sm text-primary mr-4 flex flex-row items-center"
-					><PlusIcon class="p-1 mr-1" />Manage all my notifications</nuxt-link
-				>
-				<BrandedButton :text="`Save changes`" />
+			<!-- Public view -->
+			<div v-else>
+				<!-- Avatar and description -->
+				<div class="flex mb-6">
+					<Avatar :avatar="avatar" :authorID="profile.id" :noClick="true" :size="`w-12 h-12`" />
+					<p class="text-gray7 ml-4">
+						Subscribe to {{ profile.name }}'s email notifications and manage existing subscriptions here
+					</p>
+				</div>
+				<!-- Topics -->
+				<div class="grid grid-cols-5 mb-3">
+					<h6 class="col-span-1 font-semibold text-lg">Topics</h6>
+					<div class="col-span-4 flex justify-between items-center">
+						<p>Receive email notifications for all new posts</p>
+						<label class="switch">
+							<input type="checkbox" />
+							<span class="slider round"></span>
+						</label>
+					</div>
+				</div>
+				<!-- Show list of available tags to subscribe to -->
+				<div class="grid grid-cols-5 mb-2">
+					<span class="col-span-1"></span>
+					<p class="col-span-4">Receive notifications based on tag:</p>
+				</div>
+				<div class="grid grid-cols-5 mb-6">
+					<span class="col-span-1"></span>
+					<div class="flex flex-row flex-wrap col-span-4">
+						<TagCard :tag="`placeholder1`" :noClick="true" class="mr-2 mb-2" />
+						<TagCard :tag="`placeholder2`" :noClick="true" class="mr-2 mb-2" />
+						<TagCard :tag="`placeholder3`" :noClick="true" class="mr-2 mb-2" />
+					</div>
+				</div>
+				<!-- Select email -->
+				<div class="grid grid-cols-5 mb-6">
+					<h6 class="col-span-1 font-semibold text-lg">Destination</h6>
+					<div class="col-span-4 flex flex-col">
+						<!-- For each active email -->
+						<div v-if="$store.state.session.email" class="flex flex-row items-center">
+							<input type="radio" checked />
+							<p class="ml-2">{{ $store.state.session.email }}</p>
+						</div>
+					</div>
+				</div>
+				<!-- Submit or manage subscriptions -->
+				<div class="flex justify-end items-center">
+					<nuxt-link to="/settings/notifications" class="text-sm text-primary mr-4 flex flex-row items-center"
+						><PlusIcon class="p-1 mr-1" />Manage all my notifications</nuxt-link
+					>
+					<BrandedButton :text="`Save changes`" />
+				</div>
 			</div>
 		</div>
 	</div>
--- src/components/widgets/Subscriptions.vue
+++ src/components/widgets/Subscriptions.vue
@@ -3,23 +3,51 @@
 		class="from-lightBGStart to-lightBGStop border-lightBorder mb-5 w-full rounded-lg border bg-gradient-to-r p-4 shadow-lg"
 		style="backdrop-filter: blur(10px)"
 	>
-		<h6 class="text-primary mb-2 font-semibold">Email notifications</h6>
-		<p class="text-gray5 text-sm mb-2">
-			{{ $route.params.id }} has enabled the option for future posts with these tags to be available for email delivery:
-		</p>
-		<div class="flex flex-row justify-between">
-			<div class="flex flex-row flex-wrap">
-				<TagCard :tag="`placeholder1`" class="mr-2 mb-2" />
-				<TagCard :tag="`placeholder2`" class="mr-2 mb-2" />
-				<TagCard :tag="`placeholder3`" class="mr-2 mb-2" />
+		<!-- Self-view -->
+		<div v-if="$store.state.session.id === $route.params.id">
+			<h6 class="text-primary mb-4 font-semibold">Email notifications</h6>
+			<p class="text-gray5 text-sm mb-2">
+				Display highlighted tags on your profile for readers to enable email notifications to receive your posts:
+			</p>
+			<!-- IF No tags -->
+			<button v-if="false" class="flex items-center text-primary focus:outline-none" @click="toggleNotificationsPopup">
+				<PlusIcon class="mr-2" />Add Tags
+			</button>
+			<!-- IF enabled tags -->
+			<div v-else class="grid grid-cols-5">
+				<div class="flex flex-row flex-wrap col-span-4">
+					<TagCard :tag="`green`" :noClick="true" class="mr-2 mb-2" />
+					<TagCard :tag="`eggs`" :noClick="true" class="mr-2 mb-2" />
+					<TagCard :tag="`ham`" :noClick="true" class="mr-2 mb-2" />
+				</div>
+				<div>
+					<button class="flex items-center text-primary text-sm focus:outline-none" @click="toggleNotificationsPopup">
+						Manage
+					</button>
+				</div>
 			</div>
-			<div>
-				<button
-					class="rounded-lg px-2 py-1 bg-black text-white focus:outline-none text-sm"
-					@click="toggleNotificationsPopup"
-				>
-					Create Notification
-				</button>
+		</div>
+		<!-- Public view -->
+		<div v-else>
+			<h6 class="text-primary mb-2 font-semibold">Email notifications</h6>
+			<p class="text-gray5 text-sm mb-2">
+				{{ $route.params.id }} has enabled the option for future posts with these tags to be available for email
+				delivery:
+			</p>
+			<div class="flex flex-row justify-between">
+				<div class="flex flex-row flex-wrap">
+					<TagCard :tag="`green`" class="mr-2 mb-2" />
+					<TagCard :tag="`eggs`" class="mr-2 mb-2" />
+					<TagCard :tag="`ham`" class="mr-2 mb-2" />
+				</div>
+				<div>
+					<button
+						class="rounded-lg px-2 py-1 bg-black text-white focus:outline-none text-sm"
+						@click="toggleNotificationsPopup"
+					>
+						Create Notification
+					</button>
+				</div>
 			</div>
 		</div>
 	</article>
@@ -28,12 +56,14 @@
 <script lang="ts">
 import Vue from 'vue'
 import TagCard from '@/components/Tag.vue'
+import PlusIcon from '@/components/icons/Plus.vue'
 
 interface IData {}
 
 export default Vue.extend({
 	components: {
 		TagCard,
+		PlusIcon,
 	},
 	data(): IData {
 		return {}
