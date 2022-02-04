91fed0de0f52be32c4cbfabce01a52b9b6af0480
jack dishman <me@jackdishman.com>
2022-02-04 08:58:07 -0800

    fix index redirect for logged in users

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -7,11 +7,7 @@ import Vue from 'vue'
 export default Vue.extend({
 	layout: `unauth`,
 	created() {
-		if (this.$store.state.session.id === ``) {
-			this.$router.push(`/home`)
-		} else {
-			this.$router.push(`/register`)
-		}
+		this.$router.push(`/home`)
 	},
 })
 </script>
