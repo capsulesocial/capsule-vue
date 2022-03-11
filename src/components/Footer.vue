92a8198eccc690c74bca496315103e20caa87cb1
jack dishman <me@jackdishman.com>
2022-03-10 16:47:33 -0800

    fix: link to capsule blog (#574)

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -3,7 +3,7 @@
 		<div class="mb-5 mt-1 flex flex-wrap">
 			<nuxt-link to="/help" class="pr-6">Help</nuxt-link>
 			<a href="https://www.capsule.social/writers" target="_blank" class="pr-6 mb-2">Writers</a>
-			<a href="https://blog.capsule.social/" target="_blank" class="pr-6">Blog</a>
+			<nuxt-link to="/id/capsule" class="pr-6">Blog</nuxt-link>
 			<a href="https://www.capsule.social/careers" target="_blank" class="pr-6">Careers</a>
 			<a href="https://www.iubenda.com/terms-and-conditions/40723363" target="_blank" class="pr-6">Terms</a>
 			<a href="https://www.capsule.social/about/overview" target="_blank" class="pr-6">About</a>
