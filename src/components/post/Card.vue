7fa709fcb6fced56a0aef860c001ca41c845ded3
Lilian Desvaux de Marigny <lilian.desvaux98@gmail.com>
2022-03-11 10:45:27 +0100

    style: add darkmode styling on comment icon on mobile in post card

--- src/components/post/Card.vue
+++ src/components/post/Card.vue
@@ -444,7 +444,7 @@
 									<TagPill v-for="t in post.tags" :key="t.name" :tag="t.name" class="my-2 mr-4" />
 								</div>
 								<!-- Actions buttons (Desktop) -->
-								<div class="text-gray5 dark:text-gray3 mt-1 hidden xl:flex xl:items-center">
+								<div class="text-gray5 dark:text-gray3 mt-1 hidden lg:flex lg:items-center">
 									<!-- Comment -->
 									<button
 										class="focus:outline-none text-gray5 dark:text-gray3 hover:text-primary dark:hover:text-secondary mr-4 hover:fill-primary flex items-center"
@@ -481,10 +481,10 @@
 							<TagPill v-for="t in post.tags" :key="t.name" :tag="t.name" class="my-2 mr-4" />
 						</div>
 						<!-- Comment and share (Mobile) -->
-						<div class="text-gray5 mt-1 flex xl:hidden">
+						<div class="text-gray5 dark:text-gray3 mt-1 flex lg:hidden">
 							<button
-								class="focus:outline-none text-gray5 hover:text-primary hover:fill-primary mr-2 flex items-end"
-								:class="showComments ? `text-primary` : ``"
+								class="focus:outline-none text-gray5 dark:text-gray3 hover:text-primary dark:hover:text-secondary hover:fill-primary mr-4 flex items-end"
+								:class="showComments ? `text-primary dark:text-secondary` : ``"
 								@click="toggleComments"
 							>
 								<CommentIcon :isActive="showComments" />
