731bd96cc866d371c14fbeefe3332df009e7ab7e
Lilian Desvaux de Marigny <lilian.desvaux98@gmail.com>
2022-03-22 16:44:00 +0100

    feat: add UI table for previously generated invite codes

--- src/pages/settings/account.vue
+++ src/pages/settings/account.vue
@@ -62,7 +62,6 @@
 				You have {{ inviteCodesRemaining }} invites remaining
 			</p>
 			<div class="mt-3 flex flex-col items-start xl:items-center xl:flex-row">
-				<h3 class="mr-4 font-semibold dark:text-darkPrimaryText">Invite code</h3>
 				<div class="relative mr-0 xl:mr-4 flex my-4 xl:my-0 w-full items-center xl:w-2/5">
 					<input
 						id="id"
@@ -91,6 +90,43 @@
 				<button v-else class="cursor-not-allowed text-gray5 dark:text-gray3 focus:outline-none text-sm">
 					Generate a new code
 				</button>
+			</div>
+			<label for="id" class="mb-4 mt-10 block pb-1 text-sm font-semibold dark:text-darkPrimaryText"
+				>Previously generated codes</label
+			>
+			<div class="flex flex-row justify-between text-sm font-semibold text-gray5 dark:text-gray3">
+				<p class="w-1/3">Code</p>
+				<p class="w-1/3">Expiration date</p>
+				<p class="w-1/3">Status</p>
+			</div>
+			<div v-for="code in inviteCodes" :key="code.exp" class="w-full">
+				<div
+					class="flex flex-row justify-between py-4 items-center"
+					:class="inviteCodes.length > 1 ? `border-b border-lightBorder` : `pb-0`"
+				>
+					<p class="w-1/3 text-primary dark:text-secondary">{{ code.code }}</p>
+					<p class="w-1/3 text-gray7 dark:text-gray2">{{ $formatDate(code.exp) }}</p>
+					<div class="w-1/3">
+						<p
+							v-if="checkAvailablity(code.exp, code.used) === `used`"
+							class="text-negative px-3 py-1 bg-negative bg-opacity-10 rounded-3xl border border-negative w-min"
+						>
+							Used
+						</p>
+						<p
+							v-else-if="checkAvailablity(code.exp, code.used) === `exp`"
+							class="text-negative px-3 py-1 bg-negative bg-opacity-10 rounded-3xl border border-negative w-min"
+						>
+							Expired
+						</p>
+						<p
+							v-else
+							class="text-positive px-3 py-1 bg-positive bg-opacity-10 rounded-3xl border border-positive w-min"
+						>
+							Valid
+						</p>
+					</div>
+				</div>
 			</div>
 		</div>
 	</main>
@@ -213,6 +249,16 @@ export default Vue.extend({
 			} catch (err: any) {
 				this.$toastError(err)
 				throw new Error(err)
+			}
+		},
+		checkAvailablity(exp: number, used: boolean) {
+			const now = Date.now()
+			if (used) {
+				return `used`
+			} else if (exp < now) {
+				return `exp`
+			} else {
+				return `valid`
 			}
 		},
 		redirectProfile() {
