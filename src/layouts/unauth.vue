e2e835a6cad71d8d2533d31583b96dbe964ef41d
Lilian Desvaux de Marigny <lilian.desvaux98@gmail.com>
2022-03-22 19:32:36 +0100

    fix: layout to allow srcolling of all the content on small screens (#666)
    * fix: layout to allow srcolling of all the content on small screens
    
    * style: add some padding to the footer
    
    * fix: adjust spacing for register CTAs
    
    * style: adjust unauth bg image

--- src/layouts/unauth.vue
+++ src/layouts/unauth.vue
@@ -1,6 +1,6 @@
 <template>
 	<main
-		class="bg-img m-0 h-screen overflow-y-hidden p-0 bg-lightMainBG dark:bg-darkBG"
+		class="bg-img-unauth m-0 h-screen overflow-y-hidden p-0 bg-lightMainBG dark:bg-darkBG"
 		:style="{ backgroundImage: `url(${require(`@/assets/images/brand/auth.webp`)})` }"
 	>
 		<Nuxt />
