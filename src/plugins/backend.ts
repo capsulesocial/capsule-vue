63c1d8926b85eabc5f9003f9ea748db1899ba132
Christos Panagiotakopoulos <chrispanag@gmail.com>
2022-03-25 11:49:26 +0100

    refactor: remove unnecessary console.logs

--- src/plugins/backend.ts
+++ src/plugins/backend.ts
@@ -4,11 +4,7 @@ import { initContract, initNear, initWalletConnection } from '@/backend/near'
 
 const backend: Plugin = async (context) => {
 	try {
-		// eslint-disable-next-line no-console
-		console.log(`Initialising IPFS and NEAR`)
 		await Promise.all([initIPFS(), initNear()])
-		// eslint-disable-next-line no-console
-		console.log(`IPFS and NEAR initialised!`)
 		initWalletConnection()
 
 		const accountId = window.localStorage.getItem(`accountId`)
