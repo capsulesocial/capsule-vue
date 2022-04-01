6b1afa40d718c817fb7e2c8e49dc8a8a685933bc
jack dishman <me@jackdishman.com>
2022-04-01 15:06:25 -0700

    fix: photo popup (#744)

--- src/components/popups/Image.vue
+++ src/components/popups/Image.vue
@@ -1,7 +1,7 @@
 <template>
 	<!-- Photo popup -->
 	<div
-		class="popup bg-darkBG dark:bg-gray5 modal-animation fixed top-0 bottom-0 left-0 right-0 z-40 h-screen w-full bg-opacity-50 items-center justify-center dark:bg-opacity-50 pt-24"
+		class="popup bg-darkBG dark:bg-gray5 modal-animation fixed top-0 bottom-0 left-0 right-0 z-40 h-screen w-full bg-opacity-50 items-center flex justify-center dark:bg-opacity-50 pt-24"
 		@click="closePopup"
 	>
 		<img :src="image" class="modal-content rounded-lg" />
