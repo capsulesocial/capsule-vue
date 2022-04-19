b21f0ce794216d551b331b09911b5d28b81bf37c
Nadim Kobeissi <nadim@symbolic.software>
2022-04-19 12:42:35 +0200

    feat: content policy page
    @jackdishman, I will need your help styling this page. Could you please
    adopt the appropriate stylings? Especially for <li>s which are currently
    totally unstyled.

---
+++ src/layouts/contentpolicy.vue
@@ -0,0 +1,115 @@
+<template>
+	<main
+		class="bg-img m-0 h-screen overflow-y-hidden p-0 bg-lightBG dark:bg-darkBG"
+		:style="
+			$colorMode.dark
+				? {
+						backgroundImage: `url(` + bgImage.dark + `)`,
+				  }
+				: {
+						backgroundImage: `url(` + bgImage.light + `)`,
+				  }
+		"
+	>
+		<!-- Wrapper -->
+		<div class="flex w-full justify-center">
+			<div class="flex w-full lg:w-11/12 xl:w-10/12 max-w-1220 flex-col">
+				<!-- Header -->
+				<Header :avatar="avatar" />
+				<!-- Body -->
+				<div>
+					<!-- Content -->
+					<section class="relative flex flex-row">
+						<nuxt-child
+							style="backdrop-filter: blur(10px); min-height: calc(100vh - 60px); height: calc(100vh - 60px)"
+							class="lg:w-7.5 from-lightBGStart to-lightBGStop dark:from-darkBGStart dark:to-darkBGStop border-lightBorder modal-animation box-border w-full overflow-y-auto rounded-lg border bg-gradient-to-r shadow-lg"
+							:class="showWidgets ? `` : `z-10`"
+						/>
+						<!-- Widgets -->
+						<aside
+							class="w-5/12 -mr-5 modal-animation -mt-4 hidden overflow-y-auto p-4 lg:block"
+							:class="showWidgets ? `z-10` : ``"
+							style="min-height: calc(100vh - 70px); height: calc(100vh - 70px)"
+						>
+							<SupportWidget />
+							<ReportWidget />
+							<ResourcesWidget v-if="this.$store.state.session.id !== ``" @overlay="openOnboarding" />
+							<Footer />
+						</aside>
+					</section>
+				</div>
+			</div>
+		</div>
+		<!-- Onboarding Wizard -->
+		<OnboardingWizard v-if="displayOnboarding" @closePopup="openOnboarding" />
+	</main>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import Header from '@/components/Header.vue'
+import Footer from '@/components/Footer.vue'
+import SupportWidget from '@/components/widgets/Support.vue'
+import ReportWidget from '@/components/widgets/Report.vue'
+import ResourcesWidget from '@/components/widgets/Resources.vue'
+
+import { IBackground, backgrounds } from '@/config/backgrounds'
+import { getProfile, Profile } from '@/backend/profile'
+import { getPhotoFromIPFS } from '@/backend/getPhoto'
+
+interface IData {
+	profile: Profile | null
+	avatar: string | ArrayBuffer | null
+	showWidgets: boolean
+	displayOnboarding: boolean
+	bgImage: IBackground
+}
+
+export default Vue.extend({
+	components: {
+		Header,
+		Footer,
+		SupportWidget,
+		ResourcesWidget,
+		ReportWidget,
+		OnboardingWizard: () => import(`@/components/popups/OnboardingWizard.vue`),
+	},
+	middleware: `auth`,
+	data(): IData {
+		return {
+			profile: null,
+			avatar: null,
+			showWidgets: false,
+			displayOnboarding: false,
+			bgImage: backgrounds[0],
+		}
+	},
+	async created() {
+		// Set color mode
+		this.$setColorMode(this.$store.state.settings.mode)
+		this.$setColor(this.$store.state.settings.color)
+		// unauth
+		if (this.$store.state.session.id === ``) {
+			return
+		}
+		// get logged in profile
+		const { profile } = await getProfile(this.$store.state.session.id)
+		this.profile = profile
+		this.bgImage = this.$getBGImage(this.profile?.background, `local`)
+		// Get avatar
+		if (this.profile && this.profile.avatar.length > 1) {
+			getPhotoFromIPFS(this.profile.avatar).then((p) => {
+				this.avatar = p
+			})
+		}
+	},
+	methods: {
+		toggleZIndex() {
+			this.showWidgets = !this.showWidgets
+		},
+		openOnboarding() {
+			this.displayOnboarding = !this.displayOnboarding
+		},
+	},
+})
+</script>
---
+++ src/pages/contentpolicy/index.vue
@@ -0,0 +1,226 @@
+<template>
+	<main class="xl:w-750 w-full">
+		<section>
+			<!-- Page title -->
+			<article class="px-4 py-4 xl:px-6 xl:py-5">
+				<h1 class="text-primary dark:text-secondary text-3xl font-semibold xl:text-4xl">
+					Content Policy &amp; Roadmap
+				</h1>
+			</article>
+			<div class="container px-4 pb-6 xl:px-6">
+				<h2 class="text-primary dark:text-secondary mb-6 mt-4 text-xl font-semibold">
+					Blogchain’s mission is to bee a decentralized discourse platform, allowing participants to benefit from
+					stronger guarantees against censorship.
+				</h2>
+				<article class="message mb-4 w-full xl:w-10/12">
+					<h2 class="text-lightPrimaryText dark:text-darkPrimaryText w-10/12 text-base font-semibold">
+						Why We Built Blogchain
+					</h2>
+					<p class="message-content py-2 text-sm text-sm">
+						We built Blogchain because we believe that freedom of thought and broad intellectual discourse should be the
+						norm, not the exception. We think that the best response to a disagreeable political opinion is more
+						discourse, that the best answer to a problematic scientific result is more science, and so on.
+					</p>
+					<p class="message-content py-2 text-sm text-sm">
+						However, building a platform for free expression does not mean we are championing literally all speech.
+						There are a few types of speech that simply aren’t appropriate on our platform. Such speech is defined below
+						and will be moderated off the platform.
+					</p>
+					<p class="message-content py-2 text-sm text-sm">
+						<strong
+							>Let’s talk in real, concrete examples regarding speech that we want to make more resilient, speech that
+							we have seen censored on other platforms and that would remain on Blogchain.</strong
+						>
+						In 2021, both Facebook and Twitter instituted policies immediately banning and censoring any account that
+						discussed the possibility that the COVID-19 pandemic may have originated from a failed laboratory
+						experiment. We do not have a concrete position on this topic and don’t believe that our opinion on this lab
+						leak theory should matter.
+					</p>
+					<p class="message-content py-2 text-sm text-sm">
+						Nevertheless, we did find it extremely troubling that all speech on this issue was completely restricted by
+						major social media platforms, all the way up until the Biden administration issued a statement saying that
+						the possibility of a “lab leak” was being investigated as legitimate. In the words of the Wall Street
+						Journal’s editorial board:
+					</p>
+					<p class="message-content py-2 text-sm text-sm">
+						<em
+							>Facebook acts in tandem with government on speech control. Question: When does “misinformation” stop
+							being misinformation on social media? Answer: When Democratic government authorities give permission.</em
+						>
+					</p>
+					<p class="message-content py-2 text-sm text-sm">
+						The issue here for us is the freedom to discuss pertinent intellectual topics without the implicit
+						requirement of permission from government authorities. Unfortunately, the above example wasn’t the only one
+						to occur in 2021.
+					</p>
+					<p class="message-content py-2 text-sm text-sm">
+						<strong>Let’s talk in real, concrete examples regarding speech that aren’t why we built Blogchain.</strong>
+						Our commitment to free discourse and freedom of expression does cover intellectual expression in the
+						broadest sense. But we simply won’t dedicate our time or the time of our team to uphold speech that doesn’t
+						carry value: threats, harassment, intimidation, extortion, and violence are not why we’re here.
+					</p>
+					<p class="message-content py-2 text-sm text-sm">
+						To be blunt: we’re not building the next 4chan. We have nothing against 4chan and respect what it
+						contributes to the Internet (for better or for worse) — but it simply is not what we, ourselves, have set
+						out to build.
+					</p>
+					<p class="message-content py-2 text-sm text-sm">
+						We are providing unmatched guarantees of content resilience, backed by our decentralized technology, that
+						applies to all content posted, regardless if we delist it from our Blogchain platform.
+					</p>
+					<p class="message-content py-2 text-sm text-sm">
+						We are here for the eccentric scientist, even if they are mistaken, so that they can express themselves and
+						be debated and perhaps corrected. We are here for the next Edward Snowden, even if their disclosures cause
+						upheaval. We are not here for the next account that wants to incite violence against a certain group of
+						people or post racy memes. It’s just not why we chose to spend our time building this technology and this
+						platform.
+					</p>
+					<h2 class="text-lightPrimaryText dark:text-darkPrimaryText w-10/12 text-base font-semibold">
+						Content Resilience Guarantees
+					</h2>
+					<p class="message-content py-2 text-sm text-sm">
+						What we're building with Blogchain takes advantage of decentralization technology in order to allow you to
+						retain more control over your content.
+						<strong
+							>Blogchain’s decentralized design guarantees that content will remain peered across decentralized IPFS
+							nodes no matter what — including if we decide to delist it from Blogchain.</strong
+						>
+					</p>
+					<p class="message-content py-2 text-sm text-sm">
+						<strong
+							>Blogchain also guarantees that any moderation action that we take will need to be done via smart
+							contract, providing an authenticated, transparent ledger of all account actions, thereby prohibiting
+							platform abuses such as "shadow banning."</strong
+						>
+					</p>
+					<p class="message-content py-2 text-sm text-sm">
+						In these ways, Blogchain safeguards writers and readers against platform abuses that could potentially
+						originate from us, the creators of the platform. This unique feature of Blogchain is unmatched by
+						traditional online publishing platforms and is one that provides stronger assurances to readers and writers
+						against internal corruption, dishonesty, deplatforming, and censorship.
+					</p>
+					<p class="message-content py-2 text-sm text-sm">
+						We will soon be building tools that allow any Blogchain user to quickly explore and view all Blogchain
+						moderation activity — all of which is necessarily performed via blockchain smart contract operations — for
+						an unmatched level of transparency.
+					</p>
+					<h2 class="text-lightPrimaryText dark:text-darkPrimaryText w-10/12 text-base font-semibold">
+						Four Generations of Content Policy
+					</h2>
+					<p class="message-content py-2 text-sm text-sm">
+						This document refers to “Generation 1” of Blogchain’s content policy. Generation 1 policies are, relative to
+						future generations, the most centralized approach we’ll be taking to moderation. The idea is to be
+						conservative at first at first, and to expand it as we continue to test the platform, eliminate bugs, better
+						understand what we’re building, and actually have time to implement more complex features.
+					</p>
+					<p class="message-content py-2 text-sm text-sm">Here’s an essential overview:</p>
+					<ul>
+						<li>
+							<strong>Generation 1</strong> (this policy document) outlines a content policy that is enforced by Capsule
+							Social and Blogchain admins themselves, in a relatively centralized fashion. This is counter-balanced by
+							all moderation operations being publicly logged on a smart contract and with all content remaining
+							available across IPFS nodes outside of Blogchain, even if the content is moderated and delisted on
+							Blogchain itself.
+						</li>
+						<li>
+							<strong>Generations 2, 3 and 4</strong> will bring in progressively more decentralized elements into
+							content moderation:
+							<strong>juries, community editors that users can subscribe to, community-based moderation</strong>, and
+							more. We will work towards these features as Blogchain matures, and as we become confident in our
+							understanding of what works best for what is essentially a community-run democracy that we’d like to build
+							on the platform.
+						</li>
+					</ul>
+					<h2 class="text-lightPrimaryText dark:text-darkPrimaryText w-10/12 text-base font-semibold">
+						Delisting Content
+					</h2>
+					<p class="message-content py-2 text-sm text-sm">
+						Delisting content refers to an operation whereby a Blogchain user is locked out of the platform and some or
+						all of their content is rendered non-discoverable through the Blogchain interface.
+					</p>
+					<p class="message-content py-2 text-sm text-sm">
+						Blogchain nodes operated by Capsule Social won’t mirror delisted content through their IPFS and OrbitDB
+						subcomponents. However, the content will remain available if provided by independent IPFS nodes anywhere on
+						the Internet (outside of Blogchain) and can even be accessed through Blogchain software if the IPFS CID of
+						the content is entered directly into the platform (for example, through the URL in the web browser address
+						bar).
+					</p>
+					<p class="message-content py-2 text-sm text-sm">
+						Content may only be delisted through a smart contract call, which must include the following information:
+					</p>
+					<ul>
+						<li>The username that is being locked.</li>
+						<li>The IPFS CID of the post that led to the delisting.</li>
+						<li>A timestamp.</li>
+						<li>Classification code for delisting reason.</li>
+					</ul>
+					<p class="message-content py-2 text-sm text-sm">
+						The delisted CIDs will be available via an OrbitDB log which is also publicly verifiable.
+					</p>
+					<h2 class="text-lightPrimaryText dark:text-darkPrimaryText w-10/12 text-base font-semibold">
+						Delistable Content
+					</h2>
+					<p class="message-content py-2 text-sm text-sm">
+						“Delistable Content” refers to types of content that are against the Blogchain Content Policy. Delistable
+						content categories are currently defined as follows:
+					</p>
+					<ul>
+						<li>
+							Content that is deemed illegal under United States law or Delaware state law, where Capsule Social, Inc.
+							is based. This includes terrorism, fraud, extortion, and many types of pornography or non-consensual
+							intimate imagery (NCII).
+						</li>
+						<li>
+							Specifically targeted, real threats of violence or incitement to violence, including wishing or hoping
+							that someone experiences physical harm. Persistent, malicious, targeted harassment or incitement to
+							harassment. Promoting or encouraging suicide.
+						</li>
+						<li>
+							Pornography or sexually exploitative content. We do allow depictions of nudity for artistic, journalistic,
+							or related purposes, as well as erotic literature. Users may be asked to self-identify non-porn adult
+							content and may have their accounts locked if they do not identify content correctly.
+						</li>
+						<li>Extremely gruesome, violent content, or content glorifying violence.</li>
+						<li>
+							Non-consensually posting an individual’s confidential personal information such as, for example, home
+							address, passport number, or social security number (”doxxing”).
+						</li>
+						<li>Any content that is the result of a software or platform error or vulnerability.</li>
+						<li>Plagiarism or the impersonation of any individual, group, or organization.</li>
+					</ul>
+					<p class="message-content py-2 text-sm text-sm">
+						Spam is also considered delistable content. Spam may take many forms, including but not limited to:
+						repeated, unwanted, and/or unsolicited actions, automated or manual, that negatively affect users, groups,
+						and/or the Blogchain platform itself; Content that is designed to further unlawful acts (such as phishing)
+						or mislead recipients as to the source of the material (such as spoofing); Commercially-motivated spam that
+						typically aims to drive traffic from Blogchain over to another website, service or initiative through
+						backlinking or other inauthentic methods; Inauthentic engagements that try to make channels or content
+						appear more popular than they are; Coordinated activity that attempts to artificially influence opinion
+						through the use of multiple accounts, fake accounts, and/or scripting or automation.
+					</p>
+				</article>
+			</div>
+		</section>
+	</main>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+	layout: `contentpolicy`,
+	head() {
+		return {
+			title: `Content Policy &amp; Roadmap - Blogchain`,
+			meta: [
+				{
+					hid: `contentpolicy`,
+					name: `contentpolicy`,
+					content: `Content Policy &amp; Roadmap - Blogchain`,
+				},
+			],
+		}
+	},
+	methods: {},
+})
+</script>
