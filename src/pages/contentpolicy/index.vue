8f41e74c6ef9281d500815b40e8793885ad1cbb5
jackdishman <me@jackdishman.com>
2022-04-20 10:07:07 -0700

    fix: darkmode for content policy and typography size and spacing adjustments

--- src/pages/contentpolicy/index.vue
+++ src/pages/contentpolicy/index.vue
@@ -3,12 +3,12 @@
 		<section>
 			<!-- Page title -->
 			<article class="px-4 py-4 xl:px-6 xl:py-5">
-				<h1 class="text-primary dark:text-secondary text-3xl font-semibold xl:text-4xl">
+				<h1 class="text-lightPrimaryText dark:text-darkPrimaryText text-3xl font-semibold xl:text-4xl">
 					Content Policy &amp; Roadmap
 				</h1>
 			</article>
 			<div class="container px-4 pb-6 xl:px-6">
-				<h2 class="text-primary dark:text-secondary mb-6 mt-4 text-xl font-semibold py-2">
+				<h2 class="text-primary mb-6 mt-4 text-xl font-semibold">
 					Blogchain’s mission is to be a decentralized discourse platform, allowing participants to benefit from
 					stronger guarantees against censorship.
 				</h2>
@@ -16,17 +16,17 @@
 					<h2 class="text-lightPrimaryText dark:text-darkPrimaryText text-h2 font-semibold py-2">
 						Why We Built Blogchain
 					</h2>
-					<p class="message-content py-2 text-base text-justify">
+					<p class="message-content text-gray5 dark:text-gray3 py-2 text-sm text-justify">
 						We built Blogchain because we believe that freedom of thought and broad intellectual discourse should be the
 						norm, not the exception. We think that the best response to a disagreeable political opinion is more
 						discourse, that the best answer to a problematic scientific result is more science, and so on.
 					</p>
-					<p class="message-content py-2 text-base text-justify">
+					<p class="message-content text-gray5 dark:text-gray3 py-2 text-sm text-justify">
 						However, building a platform for free expression does not mean we are championing literally all speech.
 						There are a few types of speech that simply aren’t appropriate on our platform. Such speech is defined below
 						and will be moderated off the platform.
 					</p>
-					<p class="message-content py-2 text-base text-justify">
+					<p class="message-content text-gray5 dark:text-gray3 py-2 text-sm text-justify">
 						<strong
 							>Let’s talk in real, concrete examples regarding speech that we want to make more resilient, speech that
 							we have seen censored on other platforms and that would remain on Blogchain.</strong
@@ -36,39 +36,39 @@
 						experiment. We do not have a concrete position on this topic and don’t believe that our opinion on this lab
 						leak theory should matter.
 					</p>
-					<p class="message-content py-2 text-base text-justify">
+					<p class="message-content text-gray5 dark:text-gray3 py-2 text-sm text-justify">
 						Nevertheless, we did find it extremely troubling that all speech on this issue was completely restricted by
 						major social media platforms, all the way up until the Biden administration issued a statement saying that
 						the possibility of a “lab leak” was being investigated as legitimate. In the words of the Wall Street
 						Journal’s editorial board:
 					</p>
-					<p class="message-content py-2 text-base text-justify">
+					<p class="message-content text-gray5 dark:text-gray3 py-2 text-sm text-justify">
 						<em
 							>Facebook acts in tandem with government on speech control. Question: When does “misinformation” stop
 							being misinformation on social media? Answer: When Democratic government authorities give permission.</em
 						>
 					</p>
-					<p class="message-content py-2 text-base text-justify">
+					<p class="message-content text-gray5 dark:text-gray3 py-2 text-sm text-justify">
 						The issue here for us is the freedom to discuss pertinent intellectual topics without the implicit
 						requirement of permission from government authorities. Unfortunately, the above example wasn’t the only one
 						to occur in 2021.
 					</p>
-					<p class="message-content py-2 text-base text-justify">
+					<p class="message-content text-gray5 dark:text-gray3 py-2 text-sm text-justify">
 						<strong>Let’s talk in real, concrete examples regarding speech that aren’t why we built Blogchain.</strong>
 						Our commitment to free discourse and freedom of expression does cover intellectual expression in the
 						broadest sense. But we simply won’t dedicate our time or the time of our team to uphold speech that doesn’t
 						carry value: threats, harassment, intimidation, extortion, and violence are not why we’re here.
 					</p>
-					<p class="message-content py-2 text-base text-justify">
+					<p class="message-content text-gray5 dark:text-gray3 py-2 text-sm text-justify">
 						To be blunt: we’re not building the next 4chan. We have nothing against 4chan and respect what it
 						contributes to the Internet (for better or for worse) — but it simply is not what we, ourselves, have set
 						out to build.
 					</p>
-					<p class="message-content py-2 text-base text-justify">
+					<p class="message-content text-gray5 dark:text-gray3 py-2 text-sm text-justify">
 						We are providing unmatched guarantees of content resilience, backed by our decentralized technology, that
 						applies to all content posted, regardless if we delist it from our Blogchain platform.
 					</p>
-					<p class="message-content py-2 text-base text-justify">
+					<p class="message-content text-gray5 dark:text-gray3 py-2 text-sm text-justify">
 						We are here for the eccentric scientist, even if they are mistaken, so that they can express themselves and
 						be debated and perhaps corrected. We are here for the next Edward Snowden, even if their disclosures cause
 						upheaval. We are not here for the next account that wants to incite violence against a certain group of
@@ -78,7 +78,7 @@
 					<h2 class="text-lightPrimaryText dark:text-darkPrimaryText text-h2 font-semibold py-2">
 						Content Resilience Guarantees
 					</h2>
-					<p class="message-content py-2 text-base text-justify">
+					<p class="message-content text-gray5 dark:text-gray3 py-2 text-sm text-justify">
 						What we're building with Blogchain takes advantage of decentralization technology in order to allow you to
 						retain more control over your content.
 						<strong
@@ -86,20 +86,20 @@
 							nodes no matter what — including if we decide to delist it from Blogchain.</strong
 						>
 					</p>
-					<p class="message-content py-2 text-base text-justify">
+					<p class="message-content text-gray5 dark:text-gray3 py-2 text-sm text-justify">
 						<strong
 							>Blogchain also guarantees that any moderation action that we take will need to be done via smart
 							contract, providing an authenticated, transparent ledger of all account actions, thereby prohibiting
 							platform abuses such as "shadow banning."</strong
 						>
 					</p>
-					<p class="message-content py-2 text-base text-justify">
+					<p class="message-content text-gray5 dark:text-gray3 py-2 text-sm text-justify">
 						In these ways, Blogchain safeguards writers and readers against platform abuses that could potentially
 						originate from us, the creators of the platform. This unique feature of Blogchain is unmatched by
 						traditional online publishing platforms and is one that provides stronger assurances to readers and writers
 						against internal corruption, dishonesty, deplatforming, and censorship.
 					</p>
-					<p class="message-content py-2 text-base text-justify">
+					<p class="message-content text-gray5 dark:text-gray3 py-2 text-sm text-justify">
 						We will soon be building tools that allow any Blogchain user to quickly explore and view all Blogchain
 						moderation activity — all of which is necessarily performed via blockchain smart contract operations — for
 						an unmatched level of transparency.
@@ -107,14 +107,16 @@
 					<h2 class="text-lightPrimaryText dark:text-darkPrimaryText text-h2 font-semibold py-2">
 						Four Generations of Content Policy
 					</h2>
-					<p class="message-content py-2 text-base text-justify">
+					<p class="message-content text-gray5 dark:text-gray3 py-2 text-sm text-justify">
 						This document refers to “Generation 1” of Blogchain’s content policy. Generation 1 policies are, relative to
 						future generations, the most centralized approach we’ll be taking to moderation. The idea is to be
 						conservative at first at first, and to expand it as we continue to test the platform, eliminate bugs, better
 						understand what we’re building, and actually have time to implement more complex features.
 					</p>
-					<p class="message-content py-2 text-base text-justify">Here’s an essential overview:</p>
-					<ul class="list-disc list-inside py-2 px-5">
+					<p class="message-content text-gray5 dark:text-gray3 py-2 text-sm text-justify">
+						Here’s an essential overview:
+					</p>
+					<ul class="message-content text-gray5 dark:text-gray3 py-2 text-sm list-disc list-inside py-2 px-5">
 						<li>
 							<strong>Generation 1</strong> (this policy document) outlines a content policy that is enforced by Capsule
 							Social and Blogchain admins themselves, in a relatively centralized fashion. This is counter-balanced by
@@ -132,35 +134,35 @@
 						</li>
 					</ul>
 					<h2 class="text-lightPrimaryText dark:text-darkPrimaryText text-h2 font-semibold py-2">Delisting Content</h2>
-					<p class="message-content py-2 text-base text-justify">
+					<p class="message-content text-gray5 dark:text-gray3 py-2 text-sm text-justify">
 						Delisting content refers to an operation whereby a Blogchain user is locked out of the platform and some or
 						all of their content is rendered non-discoverable through the Blogchain interface.
 					</p>
-					<p class="message-content py-2 text-base text-justify">
+					<p class="message-content text-gray5 dark:text-gray3 py-2 text-sm text-justify">
 						Blogchain nodes operated by Capsule Social won’t mirror delisted content through their IPFS and OrbitDB
 						subcomponents. However, the content will remain available if provided by independent IPFS nodes anywhere on
 						the Internet (outside of Blogchain) and can even be accessed through Blogchain software if the IPFS CID of
 						the content is entered directly into the platform (for example, through the URL in the web browser address
 						bar).
 					</p>
-					<p class="message-content py-2 text-base text-justify">
+					<p class="message-content text-gray5 dark:text-gray3 py-2 text-sm text-justify">
 						Content may only be delisted through a smart contract call, which must include the following information:
 					</p>
-					<ul class="list-disc list-inside py-2 px-5">
+					<ul class="list-disc list-inside message-content text-gray5 dark:text-gray3 py-2 text-sm py-2 px-5">
 						<li>The username that is being locked.</li>
 						<li>The IPFS CID of the post that led to the delisting.</li>
 						<li>A timestamp.</li>
 						<li>Classification code for delisting reason.</li>
 					</ul>
-					<p class="message-content py-2 text-base text-justify">
+					<p class="message-content text-gray5 dark:text-gray3 py-2 text-sm text-justify">
 						The delisted CIDs will be available via an OrbitDB log which is also publicly verifiable.
 					</p>
 					<h2 class="text-lightPrimaryText dark:text-darkPrimaryText text-h2 font-semibold py-2">Delistable Content</h2>
-					<p class="message-content py-2 text-base text-justify">
+					<p class="message-content text-gray5 dark:text-gray3 py-2 text-sm text-justify">
 						“Delistable Content” refers to types of content that are against the Blogchain Content Policy. Delistable
 						content categories are currently defined as follows:
 					</p>
-					<ul class="list-disc list-inside py-2 px-5">
+					<ul class="list-disc list-inside message-content text-gray5 dark:text-gray3 py-2 text-sm py-2 px-5">
 						<li>
 							Content that is deemed illegal under United States law or Delaware state law, where Capsule Social, Inc.
 							is based. This includes terrorism, fraud, extortion, and many types of pornography or non-consensual
@@ -184,7 +186,7 @@
 						<li>Any content that is the result of a software or platform error or vulnerability.</li>
 						<li>Plagiarism or the impersonation of any individual, group, or organization.</li>
 					</ul>
-					<p class="message-content py-2 text-base text-justify">
+					<p class="message-content text-gray5 dark:text-gray3 py-2 text-sm text-justify">
 						Spam is also considered delistable content. Spam may take many forms, including but not limited to:
 						repeated, unwanted, and/or unsolicited actions, automated or manual, that negatively affect users, groups,
 						and/or the Blogchain platform itself; Content that is designed to further unlawful acts (such as phishing)
