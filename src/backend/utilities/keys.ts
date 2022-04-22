abfa65df1eaa89c37aabc6b016c365e575d44ab5
ashutosh1206 <ashutosh.ahelleya@gmail.com>
2022-04-22 11:05:31 +0530

    feat: remove tweetnacl as direct dependency

--- src/backend/utilities/keys.ts
+++ src/backend/utilities/keys.ts
@@ -1,7 +1,6 @@
 import { keyStores } from 'near-api-js'
 import { KeyPairEd25519 } from 'near-api-js/lib/utils'
 import { base_decode as baseDecode } from 'near-api-js/lib/utils/serialize'
-import { sign } from 'tweetnacl'
 import {
 	crypto_sign_verify_detached as cryptoSignVerifyDetached,
 	crypto_sign_detached as cryptoSignDetached,
@@ -29,11 +28,6 @@ async function getNearPrivateKey() {
 
 export async function signContent<T>(content: T) {
 	const { sk, pk } = await getNearPrivateKey()
-	const keypair = sign.keyPair.fromSecretKey(sk)
-	if (!keypair) {
-		throw new Error(`Post signing failed`)
-	}
-
 	const ec = new TextEncoder()
 	const message = ec.encode(JSON.stringify(stableOrderObj(content)))
 	return { sig: cryptoSignDetached(message, sk), publicKey: pk }
@@ -43,13 +37,4 @@ export function verifyContent<T>(content: T, signature: Uint8Array, publicKey: U
 	const ec = new TextEncoder()
 	const message = ec.encode(JSON.stringify(stableOrderObj(content)))
 	return cryptoSignVerifyDetached(signature, message, publicKey)
-}
-
-export async function getSigningKey() {
-	const { sk } = await getNearPrivateKey()
-	const keypair = sign.keyPair.fromSecretKey(sk)
-	if (!keypair) {
-		return null
-	}
-	return keypair.secretKey
 }
