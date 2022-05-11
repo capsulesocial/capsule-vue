413d4fdd4fbca676a24480f277e1969698c74e13
ashutosh1206 <ashutosh.ahelleya@gmail.com>
2022-05-11 21:51:47 +0530

    fix: remove old subs management code

--- src/backend/subscription.ts
+++ src/backend/subscription.ts
@@ -3,29 +3,6 @@ import { capsuleServer, sigValidity } from './utilities/config'
 import { uint8ArrayToHexString } from './utilities/helpers'
 import { signContent } from './utilities/keys'
 
-export interface ISubscriptionEvent {
-	action: `SUBSCRIBE` | `UNSUBSCRIBE`
-	subject: string
-	object: string
-	exp: number
-}
-
-export async function subscriptionChange(action: `SUBSCRIBE` | `UNSUBSCRIBE`, self: string, user: string) {
-	const data: ISubscriptionEvent = {
-		action,
-		subject: self,
-		object: user,
-		exp: Date.now() + sigValidity,
-	}
-
-	const { sig } = await signContent(data)
-
-	await axios.post(`${capsuleServer}/subscription/subscribe`, {
-		event: data,
-		sig: uint8ArrayToHexString(sig),
-	})
-}
-
 export async function getUserSubscriptions(self: string, includeExpired?: boolean) {
 	const data = {
 		username: self,
