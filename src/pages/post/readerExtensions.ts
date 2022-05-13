4436a22b34b49fd4d594092f11cd273eb5d90a01
Christos Panagiotakopoulos <chrispanag@gmail.com>
2022-05-13 12:54:07 +0200

    fix: take \n into account

--- src/pages/post/readerExtensions.ts
+++ src/pages/post/readerExtensions.ts
@@ -22,7 +22,10 @@ const imgRegexp = (cid: string) =>
 	)
 
 export function transformPostToTemplate(body: string, postImages?: Array<string>) {
-	let removeExecution = body.replace(/\{\{(.*)\}\}/g, (_, p1) => `<span v-html="\`{{ ${sanitizeHTML(p1)} }}\`" />`)
+	let removeExecution = body.replace(
+		/\{\{(((.)|(\n))*)\}\}/,
+		(_, p1) => `<span v-html="\`{{ ${sanitizeHTML(p1)} }}\`" />`,
+	)
 	if (!postImages) {
 		return removeExecution
 	}
