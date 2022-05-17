539c653812d76a56a67d328ca4af5c23b9987af6
Christos Panagiotakopoulos <chrispanag@gmail.com>
2022-05-17 15:19:16 +0200

    feat: change max image size to 0.9Mb

--- src/backend/utilities/imageCompression.ts
+++ src/backend/utilities/imageCompression.ts
@@ -2,7 +2,7 @@ import imageCompression from 'browser-image-compression'
 
 export function getCompressedImage(file: File) {
 	return imageCompression(file, {
-		maxSizeMB: 5,
+		maxSizeMB: 0.9,
 		maxWidthOrHeight: 1920,
 		useWebWorker: true,
 		initialQuality: 0.9,
