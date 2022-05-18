981c5af131115bc79707bf957bd4660c5bd3396f
Lilian Desvaux de Marigny <lilian.desvaux98@gmail.com>
2022-05-18 17:35:50 +0200

    style: premium settings in editor (#838)
    * style: premium settings in editor
    
    * style: add darkmode

---
+++ src/components/PremiumSwitchEditor.vue
@@ -0,0 +1,46 @@
+<template>
+	<div
+		class="relative w-14 h-7 transition duration-500 ease-in-out rounded-full cursor-pointer"
+		:class="[active ? 'bg-neutral' : 'bg-gray1 dark:bg-gray7']"
+		@click="toggleSwitch"
+	>
+		<label
+			class="absolute left-0 w-7 h-7 mb-2 transition duration-300 ease-in-out transform bg-lightBG dark:bg-darkBG border-2 rounded-full flex justify-center items-center"
+			:class="[active ? 'translate-x-full border-neutral' : 'translate-x-0 border-gray1 dark:border-gray7']"
+		>
+			<CrownIcon class="text-neutral stroke-neutral self-center w-4 h-4" />
+		</label>
+		<input type="checkbox" class="w-full h-full appearance-none focus:outline-none" />
+	</div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import CrownIcon from '@/components/icons/Crown.vue'
+
+interface IData {
+	active: boolean
+}
+export default Vue.extend({
+	components: {
+		CrownIcon,
+	},
+	props: {
+		enabled: {
+			type: Boolean,
+			default: false,
+		},
+	},
+	data(): IData {
+		return {
+			active: false,
+		}
+	},
+	methods: {
+		toggleSwitch() {
+			this.active = !this.active
+			this.$emit(`toggle`)
+		},
+	},
+})
+</script>
