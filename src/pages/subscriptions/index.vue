1c1359fe4253ea0edb94d60bb22e6c519d195763
Irshad PI <irshadshalu@users.noreply.github.com>
2022-05-18 23:02:14 +0530

    feat: subscription fetch to stores without await, fixing subs renewal date (#842)
    * feat: subscription fetch to stores, removing await from created, fixing subs renewal date
    
    * feat: moving the reset of subscription after fetch so that subscriptions dont flicker when reloading

--- src/pages/subscriptions/index.vue
+++ src/pages/subscriptions/index.vue
@@ -5,7 +5,7 @@
 		<!-- subscriptions grid -->
 		<div class="flex flex-wrap mt-4">
 			<!-- Subscription card -->
-			<SubCard v-for="s in subCards" :key="s.authorID" :s="s" />
+			<SubCard v-for="s in activeSubs" :key="s.authorID" :s="s" />
 		</div>
 	</main>
 </template>
@@ -14,12 +14,10 @@
 import Vue from 'vue'
 import { mapGetters } from 'vuex'
 import SubCard from '@/components/subscriptions/SubCard.vue'
-import { ISubscriptionResponse, ISubCardData } from '@/backend/subscription'
-import { createDefaultProfile, getProfile } from '@/backend/profile'
+import { ISubscriptionResponse } from '@/backend/subscription'
 import { namespace as SubscriptionsNamespace } from '@/store/subscriptions'
 interface IData {
 	subscriptions: ISubscriptionResponse[]
-	subCards: ISubCardData[]
 }
 
 export default Vue.extend({
@@ -29,7 +27,6 @@ export default Vue.extend({
 	data(): IData {
 		return {
 			subscriptions: [],
-			subCards: [],
 		}
 	},
 	head() {
@@ -41,30 +38,8 @@ export default Vue.extend({
 	computed: {
 		...mapGetters(SubscriptionsNamespace, [`activeSubs`, `inactiveSubs`]),
 	},
-	async created() {
-		await this.$store.dispatch(`subscriptions/fetchSubs`, this.$store.state.session.id)
-		try {
-			this.activeSubs.forEach((s: ISubscriptionResponse) => {
-				let profile = createDefaultProfile(s.authorID)
-				getProfile(s.authorID).then((fetchedProfile) => {
-					if (fetchedProfile.profile) {
-						profile = fetchedProfile.profile
-					}
-					const newSubCard = {
-						name: profile.name,
-						id: s.authorID,
-						subscriptionId: s.subscriptionId,
-						tier: s.tier.name,
-						monthlySubs: -1,
-						renewDate: s.renewDate,
-						avatar: profile.avatar,
-					}
-					this.subCards.push(newSubCard)
-				})
-			})
-		} catch {
-			this.$toastError(`Error fetching subscriptions`)
-		}
+	created() {
+		this.$store.dispatch(`subscriptions/fetchSubs`, this.$store.state.session.id)
 	},
 	methods: {},
 })
