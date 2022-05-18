a14b3674e5c5e864f0e1475d291e08f9384b0420
Irshad PI <irshadshalu@users.noreply.github.com>
2022-05-18 18:51:31 +0530

    feat: added token auth, using computed property for payments enabled, more supported currencies (#832)
    * feat: added token auth, using computed property for payments enabled, added all supported currencies
    
    * feat: added missing token auth and request files
    
    * feat: handling error with fetch payment profile call

---
+++ src/backend/tokenAuth.ts
@@ -0,0 +1,43 @@
+import axios from 'axios'
+import { capsuleServer } from './utilities/config'
+import { getExpTimestamp, uint8ArrayToHexString } from './utilities/helpers'
+import { signContent } from './utilities/keys'
+
+interface ITokenData {
+	token: string
+	exp: number
+}
+
+async function getAuthToken(username: string): Promise<ITokenData> {
+	const data = { username, action: `authentication`, exp: getExpTimestamp() }
+	const { sig } = await signContent(data)
+	const response = await axios.post(`${capsuleServer}/auth`, {
+		data,
+		sig: uint8ArrayToHexString(sig),
+	})
+	return response.data
+}
+
+export async function getToken(username: string, reset = false): Promise<string> {
+	try {
+		if (reset) {
+			throw new Error(`Token to be resetted`)
+		}
+
+		const tokenData = localStorage.getItem(`serverAuthToken`)
+		if (!tokenData) {
+			throw new Error(`No token`)
+		}
+
+		const { token, exp }: ITokenData = JSON.parse(tokenData)
+		if (Date.now() > exp - 60000) {
+			throw new Error(`Token is expired`)
+		}
+
+		return token
+	} catch (err) {
+		const { token, exp } = await getAuthToken(username)
+		localStorage.setItem(`serverAuthToken`, JSON.stringify({ token, exp }))
+		return token
+	}
+}
