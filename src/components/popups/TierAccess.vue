5c056d93a393c2518004e2eceb609b8bdb8e53eb
jack dishman <me@jackdishman.com>
2022-05-19 05:35:01 -0700

    fix: separated self subscription tiers from paymentProfiles into subs… (#844)
    * fix: separated self subscription tiers from paymentProfiles into subscriptionTiers store
    
    * fix: isolate subscription tiers into store object, keep subscriptionsEnabled, dispatch on editor

--- src/components/popups/TierAccess.vue
+++ src/components/popups/TierAccess.vue
@@ -20,7 +20,7 @@
 				<!-- Tier list -->
 				<div class="flex flex-col mt-6">
 					<button
-						v-for="t in this.$store.getters[`paymentProfile/getPaymentProfile`](this.$store.state.session.id).tiers"
+						v-for="t in this.$store.getters[`subscriptionTiers/tiers`]"
 						:key="t._id"
 						class="from-lightBGStart to-lightBGStop dark:from-darkBG dark:to-darkBG bg-gradient-to-r shadow-sm border rounded-lg w-full flex flex-row justify-between items-center p-5 my-2 transition duration-500 ease-in-out"
 						:class="
