408262039fc2eab131ef367240d689f1f30883fe
Lilian Desvaux de Marigny <lilian.desvaux98@gmail.com>
2022-05-25 13:40:37 +0200

    fix: changes subcard on subscription page to better edit the subscription

--- src/components/subscriptions/SubCard.vue
+++ src/components/subscriptions/SubCard.vue
@@ -4,18 +4,18 @@
 	>
 		<div class="w-full flex justify-end -mt-2">
 			<div class="icon relative flex items-center">
-				<button class="focus:outline-none text-gray5 dark:text-gray3 ml-2" @click.stop="toggleDropdownDownload">
+				<button class="focus:outline-none text-gray5 dark:text-gray3 ml-2" @click.stop="toggleDropdownEdit">
 					<MoreIcon />
 				</button>
 				<div
-					v-show="showDownload"
+					v-show="showEdit"
 					class="bg-lightBG dark:bg-darkBG dark:text-darkPrimaryText text-lightPrimaryText border-lightBorder modal-animation absolute z-10 flex w-44 flex-col rounded-lg border p-2 shadow-lg"
 					:class="$colorMode.dark ? `dropdownSubOpenDark` : `dropdownSubOpen`"
 					style="top: 35px; right: -5px"
 				>
-					<button class="focus:outline-none text-primary flex justify-center">
-						<DownloadIcon class="fill-current p-1" />
-						<span class="text-primary ml-1 self-center text-sm">Download receipt</span>
+					<button class="focus:outline-none text-primary flex justify-center items-center" @click="$emit(`popup`, s)">
+						<PenIcon class="fill-current h-4 w-4" />
+						<span class="text-primary ml-2 self-center text-sm">Edit Subscription</span>
 					</button>
 				</div>
 			</div>
@@ -33,20 +33,11 @@
 			<CrownIcon class="text-neutral mr-2 w-5 h-5" /><span class="truncate text-neutral">{{ s.tier.name }}</span>
 		</div>
 		<p class="text-gray5 dark:text-gray3 text-sm w-full mb-2">
-			Total number of months subscribed: <span class="font-semibold">{{ s.monthsSubbed + 1 }}</span>
+			Subscribed since: <span class="font-semibold">March, 2021</span>
 		</p>
-		<p class="text-gray5 dark:text-gray3 text-sm w-full mb-2">Months in a row: <span class="font-semibold">0</span></p>
 		<p class="text-gray5 dark:text-gray3 text-sm w-full">
 			Renewal date: <span class="font-semibold">{{ $formatDate(s.renewalDate) }}</span>
 		</p>
-		<div class="flex flex-col items-end w-full mt-4 pr-1">
-			<p class="text-primary">
-				<button class="focus:outline-none text-sm">Change billing</button>
-			</p>
-			<p class="text-negative mt-1">
-				<button class="focus:outline-none text-sm">Cancel subscription</button>
-			</p>
-		</div>
 	</div>
 </template>
 
@@ -56,16 +47,17 @@ import { getPhotoFromIPFS } from '@/backend/getPhoto'
 import Avatar from '@/components/Avatar.vue'
 import CrownIcon from '@/components/icons/Crown.vue'
 import MoreIcon from '@/components/icons/More.vue'
-import DownloadIcon from '@/components/icons/Download.vue'
+import PenIcon from '@/components/icons/Pencil.vue'
 import { ISubscriptionWithProfile } from '@/store/subscriptions'
 
 interface IData {
 	avatar: string | ArrayBuffer
-	showDownload: Boolean
+	showEdit: Boolean
+	showPopup: Boolean
 }
 
 export default Vue.extend({
-	components: { Avatar, CrownIcon, MoreIcon, DownloadIcon },
+	components: { Avatar, CrownIcon, MoreIcon, PenIcon },
 	props: {
 		s: {
 			type: Object as PropType<ISubscriptionWithProfile>,
@@ -75,7 +67,8 @@ export default Vue.extend({
 	data(): IData {
 		return {
 			avatar: ``,
-			showDownload: false,
+			showEdit: false,
+			showPopup: false,
 		}
 	},
 	async created() {
@@ -85,15 +78,18 @@ export default Vue.extend({
 		window.addEventListener(`click`, this.handleDropdown, false)
 	},
 	methods: {
-		toggleDropdownDownload() {
-			this.showDownload = !this.showDownload
+		toggleDropdownEdit() {
+			this.showEdit = !this.showEdit
+		},
+		toggleInfoPopup() {
+			this.showPopup = !this.showPopup
 		},
 		handleDropdown(e: any): void {
 			if (!e.target || e.target.parentNode === null || e.target.parentNode.classList === undefined) {
 				return
 			}
 			if (!e.target.parentNode.classList.contains(`icon`)) {
-				this.showDownload = false
+				this.showEdit = false
 			}
 		},
 	},
