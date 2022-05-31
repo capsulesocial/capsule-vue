e871f778d4eda1da1d108a2b658008d0d1d46c44
Christos Panagiotakopoulos <chrispanag@gmail.com>
2022-05-31 12:01:43 +0200

    feat: better favicons (#876)
    * feat: better favicons
    
    * fix: add more sizes

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -73,8 +73,24 @@ const config: NuxtConfig = {
 				name: `twitter:image`,
 				content: `https://blogchain.app/discover.png`,
 			},
+			{
+				hid: `msapplication-TileColor`,
+				name: `msapplication-TileColor`,
+				content: `#2d89ef`,
+			},
+			{
+				hid: `theme-color`,
+				name: `theme-color`,
+				content: `#ffffff`,
+			},
 		],
-		link: [{ rel: `icon`, type: `image/x-icon`, href: `/favicon.ico` }],
+		link: [
+			{ rel: `icon`, type: `image/png`, sizes: `32x32`, href: `/favicon-32x32.png` },
+			{ rel: `icon`, type: `image/png`, sizes: `16x16`, href: `/favicon-16x16.png` },
+			{ rel: `apple-touch-icon`, sizes: `180x180`, href: `/apple-touch-icon.png` },
+			{ rel: `manifest`, href: `/site.webmanifest` },
+			{ rel: `mask-icon`, href: `/safari-pinned-tab.svg`, color: `#4a4a4a` },
+		],
 		script: [],
 	},
 	loadingIndicator: {
