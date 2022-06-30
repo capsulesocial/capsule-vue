6b4f4d2257c98a1e00e04db8fe37705ec1ad97da
Lilian Desvaux de Marigny <lilian.desvaux98@gmail.com>
2022-06-30 15:14:18 +0200

    feat: added authors list to newsletter settings

--- src/pages/settings/notifications.vue
+++ src/pages/settings/notifications.vue
@@ -5,17 +5,46 @@
 			<span class="bg-gray1 dark:bg-gray5 mr-4 rounded-full p-1"><ChevronLeft /></span>
 			<h6 class="font-semibold dark:text-darkPrimaryText">All Settings</h6>
 		</nuxt-link>
-		<!-- Email notifications -->
+		<!-- Email newsletter -->
 		<h3 class="text-lightPrimaryText dark:text-darkPrimaryText pb-2 text-base font-semibold">Email newsletter</h3>
 		<p class="text-gray5 dark:text-gray3 text-sm">
-			You are currently receiving email notifications from the following authors:
+			You are currently receiving email newsletter from the following authors:
 		</p>
+		<div v-if="!isLoading" class="flex flex-wrap mt-4">
+			<ProfilePreviewCard v-for="profiles in 4" :key="profiles.id" :profile="author1" class="pb-4 mx-1 mb-2" />
+		</div>
 	</main>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
+import ProfilePreviewCard from '@/components/ProfilePreviewCard.vue'
+import ChevronLeft from '@/components/icons/ChevronLeft.vue'
+import { createDefaultProfile, getProfile, Profile } from '@/backend/profile'
+
+interface IData {
+	author1: Profile | null
+	isLoading: boolean
+}
 export default Vue.extend({
+	components: { ProfilePreviewCard, ChevronLeft },
 	layout: `settings`,
+	data(): IData {
+		return {
+			author1: null,
+			isLoading: true,
+		}
+	},
+	created() {
+		// Get author profile
+		this.author1 = createDefaultProfile(`nairobi`)
+		getProfile(`nairobi`).then((p) => {
+			const { profile } = p
+			if (profile) {
+				this.author1 = profile
+			}
+			this.isLoading = false
+		})
+	},
 })
 </script>
