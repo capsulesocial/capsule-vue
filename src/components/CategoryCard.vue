5d9219b64220184a77b3ba3227aa8c5f2997dc87
jackdishman <me@jackdishman.com>
2022-07-11 11:31:38 -0400

    perf: made discover background images nuxt images

--- src/components/CategoryCard.vue
+++ src/components/CategoryCard.vue
@@ -5,12 +5,23 @@
 			:to="'/discover/' + text"
 			class="bg-primary flex w-full transform items-end overflow-hidden rounded-lg shadow-lg transition duration-500 ease-in-out hover:scale-105"
 			:style="{
-				background: `linear-gradient(180deg, rgba(0, 0, 0, 0) 50%, rgba(0, 0, 0, 0.8) 100%), url(` + bgImage + `)`,
-				backgroundSize: 'cover',
 				height: '200px',
 			}"
 		>
-			<!-- <img v-if="bgImage !== ``" :src="bgImage" :alt="text" class="absolute left-0 top-0 object-contain w-full" /> -->
+			<nuxt-img
+				v-if="bgImage !== ``"
+				:src="bgImage"
+				:alt="text"
+				class="absolute left-0 top-0 object-contain w-full"
+				loading="lazy"
+			/>
+			<div
+				class="absolute z-10 left-0 top-0 object-contain w-full h-full"
+				:style="{
+					background: `linear-gradient(180deg, rgba(0, 0, 0, 0) 50%, rgba(0, 0, 0, 0.8) 100%)`,
+					backgroundSize: 'cover',
+				}"
+			></div>
 			<div class="flex flex-col p-4 font-sans">
 				<h4 class="z-20 text-2xl font-extrabold capitalize text-white">
 					{{ text.replace(`-`, ` `) }}
