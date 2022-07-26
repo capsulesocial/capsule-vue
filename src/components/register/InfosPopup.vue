47686e3b78582313f57810cabdcaf95e8a72b6d0
John Philip <developerphilo@gmail.com>
2022-07-26 13:38:44 +0300

    fix: update signup methods and

--- src/components/register/InfosPopup.vue
+++ src/components/register/InfosPopup.vue
@@ -16,40 +16,39 @@
 						<h4 class="text-lg font-semibold dark:text-darkPrimaryText">Sign up with Google</h4>
 						<p class="my-2 text-gray5 dark:text-gray3">
 							Signing up on Blogchain requires writing data on the blockchain through transactions on a smart contract
-							with small amounts of NEAR token. You’ll need to provide your phone number to verify your human identity
-							and enable Capsule Social to sponsor you with NEAR tokens. Blogchain and Capsule Social will not store or
-							track any information from your phone number or Google account. To log in, use your Google account or
-							import your Blogchain private key under “Import private key”.
+							with small amounts of NEAR token. You’ll need to solve the captcha to verify your human identity and
+							enable Capsule Social to sponsor you with NEAR tokens. Blogchain and Capsule Social doesn't store or track
+							any information from your Google account. To log in, use your Google account or import your Blogchain
+							private key under “Import private key”.
 						</p>
 					</div>
 					<div class="flex flex-col my-4">
 						<h4 class="text-lg font-semibold dark:text-darkPrimaryText">Sign up with Discord</h4>
 						<p class="my-2 text-gray5 dark:text-gray3">
 							Signing up on Blogchain requires writing data on the blockchain through transactions on a smart contract
-							with small amounts of NEAR token. You’ll need to provide your phone number for verifying your human
-							identity and enable Capsule Social to sponsor you with NEAR tokens. Blogchain and Capsule Social will not
-							store or track information on your accounts. To log in, use your Discord account or import your Blogchain
-							private key under “Import private key”.
+							with small amounts of NEAR token. You’ll need to solve the captcha to verify your human identity and
+							enable Capsule Social to sponsor you with NEAR tokens. Blogchain and Capsule Social doesn't store or track
+							information on your accounts. To log in, use your Discord account or import your Blogchain private key
+							under “Import private key”.
 						</p>
 					</div>
 					<div class="flex flex-col my-4">
 						<h4 class="text-lg font-semibold dark:text-darkPrimaryText">Sign up with NEAR</h4>
 						<p class="my-2 text-gray5 dark:text-gray3">
-							Create an account using your NEAR wallet. In case of insufficient funds, you will be sponsored with NEAR
-							tokens for running the transaction on the smart contract and registering your account on the blockchain.
-							By signing up with NEAR, you need to download your Blogchain private key and use it to log in under
-							“Import private key”. Your phone number verification is not needed for account sign up with NEAR if you
-							have the necessary funds to complete the registration.
+							Create an account using your NEAR wallet. In case of insufficient funds, you will need to solve the
+							captcha and be sponsored with NEAR tokens enough for running the transaction on the smart contract and
+							registering your account on the blockchain. By signing up with NEAR, you need to download your Blogchain
+							private key and use it to log in under “Import private key”.
 						</p>
 					</div>
 					<div class="flex flex-col my-4">
 						<h4 class="text-lg font-semibold dark:text-darkPrimaryText">Sign up with Blogchain private key</h4>
 						<p class="my-2 text-gray5 dark:text-gray3">
-							Create an account where you self manage your account keys. You’ll need to provide your phone number to
-							verify your human identity and enable Capsule Social to sponsor you with NEAR tokens. These NEAR tokens
-							are used for performing transactions on a smart contract and registering your account on the blockchain.
-							By signing up with this method, you’ll need to download your Blogchain private key and use it to log in
-							under “Import Blogchain private key”.
+							Create an account where you self manage your account keys. You’ll need solve the captcha to verify your
+							human identity and enable Capsule Social to sponsor you with NEAR tokens. These NEAR tokens are used for
+							performing transactions on a smart contract and registering your account on the blockchain. By signing up
+							with this method, you’ll need to download your Blogchain private key and use it to log in under “Import
+							Blogchain private key”.
 						</p>
 					</div>
 				</div>
