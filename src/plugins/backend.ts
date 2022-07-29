f59c9a845b58f8121658d666fa96a2c01d7e5410
ashutosh1206 <ashutosh.ahelleya@gmail.com>
2022-07-29 13:03:17 +0530

    fix: replace testnet keystore only on mainnet

--- src/plugins/backend.ts
+++ src/plugins/backend.ts
@@ -2,11 +2,13 @@ import { Plugin } from '@nuxt/types'
 import { initIPFS } from '@/backend/utilities/ipfs'
 import { initLibSodium } from '@/backend/utilities/keys'
 import { initContract, initNear, initWalletConnection } from '@/backend/near'
+import { nearNetwork } from '@/backend/utilities/config'
 
 const backend: Plugin = async (context) => {
 	try {
 		// If the user has refreshed or backtracked etc
-		for (let i = 0; i < localStorage.length; i++) {
+		// eslint-disable-next-line no-unmodified-loop-condition
+		for (let i = 0; i < localStorage.length && nearNetwork === `mainnet`; i++) {
 			const key = localStorage.key(i)
 			if (key && key.startsWith(`near-api-js:keystore`)) {
 				if (key.endsWith(`testnet`)) {
