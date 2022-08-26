48d4c5a258cadac471c0b67e0cfe9b9b07ada6d0
Rahul Trivedi <32861078+rahultrivedi180@users.noreply.github.com>
2022-08-26 13:22:21 +0530

    feat: shareable profile link (#1151)
    * feat: add backend function to get shareable profile link
    
    * feat: fetch and replace profile url

--- src/backend/shareable_links.ts
+++ src/backend/shareable_links.ts
@@ -5,3 +5,8 @@ export async function createShareableLink(cid: string) {
 	const response = await axios.post(`${capsuleServer}/share`, { cid })
 	return response.data.data as string
 }
+
+export async function getShareableProfileLink(id: string) {
+	const response = await axios.get(`${capsuleServer}/u/${id}/url`)
+	return response.data.data as string
+}
