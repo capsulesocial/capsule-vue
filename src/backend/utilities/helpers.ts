eb592cc7d70e179e34d841e571cfc3c2bcbb7833
Rahul Trivedi <rahultrivedi180@gmail.com>
2022-09-02 14:35:56 +0530

    fix: using json-stable-stringify

--- src/backend/utilities/helpers.ts
+++ src/backend/utilities/helpers.ts
@@ -15,18 +15,6 @@ export function hexStringToUint8Array(hexString: string): Uint8Array {
 	return new Uint8Array(Buffer.from(hexString, `hex`))
 }
 
-export function stableOrderObj<T extends Record<string, any>>(obj: T): T {
-	const keys = Object.keys(obj)
-	keys.sort()
-
-	const newObj: any = {}
-	for (const key of keys) {
-		newObj[key] = obj[key]
-	}
-
-	return newObj as T
-}
-
 export function getBlobExtension(blob: Blob): string | null {
 	switch (blob.type) {
 		case `image/png`:
