a4b5c726f7daa02916be6e07b5e0ad221fbf50cc
Rahul Trivedi <rahultrivedi180@gmail.com>
2022-09-28 16:06:19 +0530

    fix: highlight code in mounted hook

--- src/components/PostPreview.vue
+++ src/components/PostPreview.vue
@@ -6,6 +6,7 @@
 
 <script lang="ts">
 import Vue from 'vue'
+import hljs from 'highlight.js'
 import { afterSanitizeAttrsHook, BASE_ALLOWED_ATTRS, BASE_ALLOWED_TAGS, sanitizeHtml } from '@/plugins/helpers'
 
 const ALLOWED_TAGS = BASE_ALLOWED_TAGS
@@ -26,9 +27,15 @@ export default Vue.extend({
 	created() {
 		afterSanitizeAttrsHook()
 	},
+	mounted() {
+		document.querySelectorAll(`pre`).forEach((block) => {
+			hljs.highlightBlock(block)
+		})
+	},
 })
 </script>
 <style>
+@import 'https://cdn.jsdelivr.net/gh/highlightjs/cdn-release@11.6.0/build/styles/monokai-sublime.min.css';
 .editable img {
 	display: block;
 	margin-left: auto;
